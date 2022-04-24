750a7125834244a1c8244ead37ecf1192fa1afa5
15227241216 <1469442737@qq.com>
2022-04-24 22:10:16 +0800

    ci: 更新配置项 跟新构建

--- packages/fighting-docs/docs/.vitepress/theme/index.ts
+++ packages/fighting-docs/docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { VPTheme } from '@vue/theme'
 import tyhUi from 'tyh-ui2'
 import 'tyh-ui2/style/index.css'
-import './code.scss'
+import './code.css'
 
 export default Object.assign({}, VPTheme, {
   enhanceApp({ app }) {
