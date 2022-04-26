566e6b2394b326b27a6c66ddb0c811ca326903b1
tyh2001 <1469442737@qq.com>
2022-04-26 12:30:22 +0800

    ci: 构建项目

---
+++ packages/conponents/button/index.ts
@@ -0,0 +1,6 @@
+import Button from './src/button'
+import { install } from '@fighting-design/utils'
+
+install(Button, Button.name)
+
+export default Button
---
+++ packages/conponents/button/src/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { prop } from './prop'
+
+defineOptions({
+  name: 'FiButton'
+})
+
+const props = defineProps(prop)
+
+</script>
