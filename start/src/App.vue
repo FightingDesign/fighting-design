ad200188f2128fbee95a37f8c9be6c500a89a5a9
15227241216 <1469442737@qq.com>
2022-04-28 23:44:36 +0800

    feat: 更新按钮组件

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <f-button disabled type="default">1212</f-button>
-  <f-button disabled type="primary">1212</f-button>
-  <f-button disabled type="success">1212</f-button>
-  <f-button disabled type="danger">1212</f-button>
-  <f-button disabled type="warning">1212</f-button>
-  <f-button disabled type="info">1212</f-button>
+  <f-button blob type="default">1212</f-button>
+  <f-button blob type="primary">1212</f-button>
+  <f-button blob type="success">1212</f-button>
+  <f-button blob type="danger">1212</f-button>
+  <f-button blob type="warning">1212</f-button>
+  <f-button blob type="info">1212</f-button>
   <br />
-  <f-button round autofocus disabled type="default" @click="aaa">1212</f-button>
-  <f-button round type="primary">1212</f-button>
-  <f-button round type="success">1212</f-button>
-  <f-button round type="danger">1212</f-button>
-  <f-button round type="warning">1212</f-button>
-  <f-button round type="info">1212</f-button>
+  <f-button :round="isR" type="default" @click="aaa">1212</f-button>
+  <f-button :round="isR" type="primary">1212</f-button>
+  <f-button :round="isR" type="success">1212</f-button>
+  <f-button :round="isR" type="danger">1212</f-button>
+  <f-button :round="isR" type="warning">1212</f-button>
+  <f-button :round="isR" type="info">1212</f-button>
 </template>
 
 <script lang="ts" setup>
 import { FButton } from '@fighting-design/components'
+import { ref } from 'vue'
+
+const isR = ref(true)
 
 function aaa(e: EventInit) {
   console.log(11)
+  isR.value = !isR.value
 }
 </script>
