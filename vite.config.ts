03a138f7e7f59c003802fb26b51a95e35d06b95a
15227241216 <1469442737@qq.com>
2022-04-30 00:54:55 +0800

    fix: 更新打包配置项

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig({
     //   entry: '/packages/fighting-design/index.ts',
     //   name: 'fighting-design'
     // },
+    // https://cn.vitejs.dev/guide/build.html#customizing-the-build
     rollupOptions: {
-      input: '/packages/fighting-design/index.ts',
-      // output: {
-      //   file: 'fighting.js',//打包到那个文件
-      //   format: 'esm' // 生成包的格式
-      // }
+      input: '/packages/fighting-design/index.ts', // 输入地址
+      output: {
+        // dir: '', // 放置所有生成的块的目录
+        // file: 'fighting.js',// 要写入的文件 打包到那个文件
+        // format: 'esm' // 生成包的格式
+      }
     }
   }
 })
