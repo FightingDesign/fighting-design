3ade612b3e6159f83809e0706289b918b68514b5
15227241216 <1469442737@qq.com>
2022-05-16 23:25:57 +0800

    chore: 更新构建的配置文件

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -4,15 +4,20 @@ import vuePlugin from 'rollup-plugin-vue' // 处理 vue 文件
 import { readdirSync } from 'fs' // 写文件
 import { resolve } from 'path'
 
+// import css from 'rollup-plugin-css-only'
+// rollup-plugin-terser
+// 如果依赖模块中存在 es 模块，需要使用 @rollup/plugin-node-resolve 插件进行转换
+
 const input = resolve(__dirname, '../packages/fighting-components') // 入口目录
-const output = resolve(__dirname, '../dist/packages') // 输入目录
+const output = resolve(__dirname, '../dist/packages') // 出口目录
 
 const config = readdirSync(input)
   .filter(name => !['env.d.ts', 'package.json', 'index.ts'].includes(name))
   .map(name => (
     {
-      input: `${input}/${name}/index.ts`,
-      external: ['vue'],
+      input: `${input}/${name}/index.ts`, // 入口文件
+      external: ['vue'], // 依赖模块
+      // 插件列表
       plugins: [
         nodeResolve(),
         vuePlugin(),
@@ -27,6 +32,7 @@ const config = readdirSync(input)
           clean: true
         })
       ],
+      // 出口配置
       output: {
         name: 'index',
         file: `${output}/${name}/index.ts`,
