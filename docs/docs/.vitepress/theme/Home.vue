033dd9082d8104cc3ec4f656190d910b61d0a6ff
tyh2001 <1469442737@qq.com>
2022-05-17 18:14:14 +0800

    docs: 更新文档首页

--- docs/docs/.vitepress/theme/Home.vue
+++ docs/docs/.vitepress/theme/Home.vue
@@ -47,48 +47,41 @@
   </div>
   <div class="footer">
     <div class="footer_box">
+      <!-- 列表选项 -->
       <div class="list">
-        <ul>
-          <h4>社区</h4>
-          <li v-for="(item, index) in community" :key="index">
-            <a target="_back" :href="item.link">{{ item.text }}</a>
-          </li>
-        </ul>
-        <ul>
-          <h4>关于我</h4>
-          <li v-for="(item, index) in my" :key="index">
-            <a target="_back" :href="item.link">{{ item.text }}</a>
-          </li>
-        </ul>
-        <ul>
-          <h4>关于我</h4>
-          <li v-for="(item, index) in works" :key="index">
-            <a target="_back" :href="item.link">{{ item.text }}</a>
-          </li>
-        </ul>
-        <ul>
-          <h4>帮助</h4>
-          <li v-for="(item, index) in help" :key="index">
-            <a target="_back" :href="item.link">{{ item.text }}</a>
+        <ul v-for="(lists, index) in bottomList" :key="index">
+          <h4>{{ lists.title }}</h4>
+          <li v-for="(list, i) in lists.item" :key="i">
+            <a target="_back" :href="list.link">{{ list.text }}</a>
           </li>
         </ul>
       </div>
-      <!-- <a
-        v-for="(item, index) in collaborator"
-        target="_blank"
-        :href="item.url"
-        :key="index"
-      >
-        <div class="userBox">
-          <img
-            draggable="false"
-            class="myPhoto"
-            :src="item.photo"
-            alt="photo"
-          />
-          <span class="userName">{{ item.name }}</span>
-        </div>
-      </a> -->
+
+      <div class="list2">
+        <h4>加入我们</h4>
+        <img src="https://tianyuhao.cn/images/weixin2.png" alt="weixin" />
+      </div>
+
+      <div class="list2">
+        <h4>合作者</h4>
+        <a
+          v-for="(item, index) in collaborator"
+          target="_blank"
+          :href="item.url"
+          :key="index"
+        >
+          <div class="userBox">
+            <img
+              draggable="false"
+              class="myPhoto"
+              :src="item.photo"
+              alt="photo"
+            />
+            <span class="userName">{{ item.name }}</span>
+          </div>
+        </a>
+      </div>
+
       <p class="code">FightingDesign | 浙ICP备2021024540号-2</p>
     </div>
   </div>
@@ -96,40 +89,57 @@
 
 <script lang="ts" setup>
 import Clipboard from 'clipboard'
-const lists: string[] = [
+
+const lists = [
   '更快的搭建',
   '更完善的组件',
   '更灵活的配置',
   '更优质的文档',
   '更强的类型校验',
   '更合适的组件库'
-]
-const collaborator: object[] = [
+] as const
+
+const bottomList = [
+  {
+    title: '社区',
+    item: [
+      { text: 'Github', link: 'https://github.com/Tyh2001/fighting-design' },
+      { text: 'NPM', link: 'https://www.npmjs.com/package/fighting-design' }
+    ]
+  },
+  {
+    title: '关于我',
+    item: [
+      { text: 'Tyh2001', link: 'https://tianyuhao.cn' },
+      { text: 'Blog', link: 'https://tianyuhao.cn/blog' }
+    ]
+  },
+  {
+    title: '其它开源',
+    item: [
+      { text: 'Tyh Ui', link: 'https://tianyuhao.cn/v3' },
+      { text: 'TsMango', link: 'https://tianyuhao.cn/mango' }
+    ]
+  },
+  {
+    title: '帮助',
+    item: [
+      {
+        text: '报告问题',
+        link: 'https://github.com/Tyh2001/fighting-design/issues'
+      }
+    ]
+  }
+] as const
+
+const collaborator = [
   {
     name: '田同学',
     photo: 'https://avatars.githubusercontent.com/u/73180970?v=4',
     url: 'https://github.com/Tyh2001'
   }
 ]
-// 社区
-const community = [
-  { text: 'Github', link: 'https://github.com/Tyh2001/fighting-design' },
-  { text: 'NPM', link: 'https://github.com/Tyh2001/fighting-design' }
-]
-const my = [
-  { text: 'Tyh2001', link: 'https://tianyuhao.cn' },
-  { text: 'Blog', link: 'https://tianyuhao.cn/blog' }
-]
-const works = [
-  { text: 'Tyh Ui', link: 'https://tianyuhao.cn/v3' },
-  { text: 'TsMango', link: 'https://tianyuhao.cn/mango' }
-]
-const help = [
-  {
-    text: '报告问题',
-    link: 'https://github.com/Tyh2001/fighting-design/issues'
-  }
-]
+
 const copyCode = (node: HTMLButtonElement): void => {
   const clipboard = new Clipboard(node)
   alert('复制成功')
@@ -144,7 +154,6 @@ const copyCode = (node: HTMLButtonElement): void => {
   .title {
     font-weight: 600;
     font-size: 26px;
-    color: rgb(17, 24, 39);
     display: flex;
     align-items: center;
     transition: 0.3s;
@@ -158,7 +167,6 @@ const copyCode = (node: HTMLButtonElement): void => {
     margin-top: 10px;
     font-size: 33px;
     font-weight: 700;
-    color: rgb(17, 24, 39);
     transition: 0.3s;
     font-weight: 500;
     line-height: 50px;
@@ -210,7 +218,6 @@ const copyCode = (node: HTMLButtonElement): void => {
       .text {
         margin-top: 30px;
         font-size: 20px;
-        color: #333;
       }
     }
   }
@@ -227,13 +234,14 @@ const copyCode = (node: HTMLButtonElement): void => {
   .footer_box {
     max-width: 1200px;
     margin: auto;
-    padding: 70px 50px;
+    padding: 40px 50px 70px 50px;
     box-sizing: border-box;
     .list {
       color: #fff;
       display: flex;
       justify-content: space-between;
       user-select: none;
+      padding: 30px 0;
       ul {
         h4 {
           font-weight: 600;
@@ -242,6 +250,38 @@ const copyCode = (node: HTMLButtonElement): void => {
         li {
           line-height: 36px;
           cursor: pointer;
+        }
+      }
+    }
+    .list2 {
+      color: #fff;
+      display: flex;
+      justify-content: space-between;
+      user-select: none;
+      padding: 30px 0;
+      flex-direction: column;
+      h4 {
+        font-weight: 600;
+        margin-bottom: 20px;
+      }
+      img {
+        width: 150px;
+      }
+      .userBox {
+        width: 150px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        .myPhoto {
+          width: 45px;
+          height: 45px;
+          border-radius: 50%;
+        }
+        .userName {
+          font-size: 20px;
+          margin-left: 10px;
         }
       }
     }
