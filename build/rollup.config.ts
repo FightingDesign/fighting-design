ef9acb7693f29def13b5f4fddb0ce62959ce5344
tyh2001 <1469442737@qq.com>
2022-05-19 11:04:51 +0800

    chore: 更新构建

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -1,19 +1,20 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 import vuePlugin from 'rollup-plugin-vue'
+import { terser } from 'rollup-plugin-terser'
 import { readdirSync } from 'fs'
 import { resolve } from 'path'
-import { terser } from 'rollup-plugin-terser'
 
 const input = resolve(__dirname, '../packages/fighting-components')
 const output = resolve(__dirname, '../dist/packages')
-const lib = resolve(__dirname, '../dist/lib')
+// const lib = resolve(__dirname, '../dist/lib')
 
 const config = readdirSync(input)
-  .filter(name => !['env.d.ts', 'package.json'].includes(name))
+  .filter(name => !['index.ts', 'package.json'].includes(name))
   .map(name => (
     {
-      input: name === 'index.ts' ? `${input}/index.ts` : `${input}/${name}/index.ts`,
+      // input: name === 'index.ts' ? `${input}/index.ts` : `${input}/${name}/index.ts`,
+      input: `${input}/${name}/index.ts`,
       external: ['vue'],
       plugins: [
         nodeResolve(),
@@ -31,9 +32,11 @@ const config = readdirSync(input)
         })
       ],
       output: {
-        name: name === 'index.ts' ? 'FightingDesign' : '',
-        file: name === 'index.ts' ? `${lib}/index.js` : `${output}/${name}/index.ts`,
-        format: name === 'index.ts' ? 'iife' : 'es',
+        // name: name === 'index.ts' ? 'FightingDesign' : '',
+        // file: name === 'index.ts' ? `${lib}/index.js` : `${output}/${name}/index.ts`,
+        // format: name === 'index.ts' ? 'iife' : 'es',
+        file: `${output}/${name}/index.ts`,
+        format: 'es',
       }
     }
   ))
