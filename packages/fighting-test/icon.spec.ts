c12492ae928f19316f38b57e6ebcffb05e098aaa
15227241216 <1469442737@qq.com>
2022-05-22 13:06:36 +0800

    test: 更新 icon 单元测试

--- packages/fighting-test/icon.spec.ts
+++ packages/fighting-test/icon.spec.ts
@@ -5,8 +5,36 @@ import { describe, expect, it } from 'vitest'
 describe('FIcon', () => {
   it('icon', () => {
     const wrapper = mount(FIcon, {
-      props: { icon: 'f-icon-credit-level-fill', fontClass: '', color: '', size: '', opacity: '' }
+      props: { icon: 'f-icon-credit-level-fill' }
     })
     expect(wrapper.classes()).toContain('f-icon-credit-level-fill')
+  })
+
+  it('fontClass', () => {
+    const wrapper = mount(FIcon, {
+      props: { fontClass: 'my-font' }
+    })
+    expect(wrapper.classes()).toContain('my-font')
+  })
+
+  it('color', () => {
+    const wrapper = mount(FIcon, {
+      props: { color: 'red' }
+    })
+    expect(wrapper.attributes('style')).toContain('color: red')
+  })
+
+  it('size', () => {
+    const wrapper = mount(FIcon, {
+      props: { size: '15px' }
+    })
+    expect(wrapper.attributes('style')).toContain('font-size: 15px')
+  })
+
+  it('opacity', () => {
+    const wrapper = mount(FIcon, {
+      props: { opacity: '0.5' }
+    })
+    expect(wrapper.attributes('style')).toContain('opacity: 0.5')
   })
 })
