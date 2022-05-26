83e8cf4d8b8f398d44c6beb6054399241f099e6a
tyh2001 <1469442737@qq.com>
2022-05-26 15:38:06 +0800

    feat: 新增图片预览操作

--- packages/fighting-components/image/src/PreviewList.vue
+++ packages/fighting-components/image/src/PreviewList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="f-image-preview">
-    <img class="f-image-showImg" :src="previewList[previewShowIndex]" />
+    <img
+      class="f-image-showImg"
+      :src="previewList[previewShowIndex]"
+      :style="{ transform: `scale(${scale}) rotate(${rotate}deg)` }"
+    />
 
     <div class="right_button">
       <f-icon size="22px" icon="f-icon-arrow-right" @click="next" />
@@ -15,6 +19,7 @@
     <div class="option">
       <f-icon size="22px" icon="f-icon-suoxiao" @click="small" />
       <f-icon size="22px" icon="f-icon-fangda" @click="big" />
+      <f-icon size="22px" icon="f-icon-column1" @click="recovery" />
       <f-icon size="22px" icon="f-icon-xuanzhuan-1" @click="turnRight" />
       <f-icon size="22px" icon="f-icon-xuanzhuan-2" @click="turnRight" />
     </div>
@@ -31,12 +36,20 @@
   const emit = defineEmits(['close'])
 
   const previewShowIndex: Ref<number> = ref<number>(prop.previewShowIndex)
+  const scale: Ref<number> = ref<number>(1)
+  const rotate: Ref<number> = ref<number>(0)
 
   const close: optionInterface = (): void => {
     emit('close', false)
   }
 
+  const recovery: optionInterface = (): void => {
+    scale.value = 1
+    rotate.value = 0
+  }
+
   const prev: optionInterface = (): void => {
+    recovery()
     if (previewShowIndex.value > 0) {
       previewShowIndex.value--
       return
@@ -45,6 +58,7 @@
   }
 
   const next: optionInterface = (): void => {
+    recovery()
     if (previewShowIndex.value < prop.previewList.length - 1) {
       previewShowIndex.value++
       return
@@ -53,18 +67,18 @@
   }
 
   const small: optionInterface = (): void => {
-    console.log('Small')
+    scale.value -= 0.2
   }
 
   const big: optionInterface = (): void => {
-    console.log('big')
+    scale.value += 0.2
   }
 
   const turnLeft: optionInterface = (): void => {
-    console.log('turnLeft')
+    rotate.value += 90
   }
 
   const turnRight: optionInterface = (): void => {
-    console.log('turnRight')
+    rotate.value -= 90
   }
 </script>
