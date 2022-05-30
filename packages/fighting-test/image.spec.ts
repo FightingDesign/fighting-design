3e22eadbbf4f149d247f077445cdb331c34dbd30
tyh2001 <1469442737@qq.com>
2022-05-30 14:36:00 +0800

    test: 更新单元测试

--- packages/fighting-test/image.spec.ts
+++ packages/fighting-test/image.spec.ts
@@ -3,5 +3,73 @@ import { describe, expect, test } from 'vitest'
 import { FImage } from '@fighting-design/fighting-components'
 
 describe('FImage', () => {
+  test('src', () => {
+    const wrapper = mount(FImage, {
+      props: { src: 'https://abc.com/1.jpg' }
+    })
+    expect(wrapper.find('img').attributes('src')).toContain('https://abc.com/1.jpg')
+  })
 
+  test('alt', () => {
+    const wrapper = mount(FImage, {
+      props: { alt: '图片' }
+    })
+    expect(wrapper.find('img').attributes('alt')).toContain('图片')
+  })
+
+  test('draggable', () => {
+    const wrapper = mount(FImage, {
+      props: { draggable: false }
+    })
+    expect(wrapper.find('img').attributes('draggable')).toContain('false')
+  })
+
+  test('width', () => {
+    const wrapper = mount(FImage, {
+      props: { width: '200px' }
+    })
+    expect(wrapper.find('img').attributes('style')).toContain('200px')
+  })
+
+  test('height', () => {
+    const wrapper = mount(FImage, {
+      props: { height: '300px' }
+    })
+    expect(wrapper.find('img').attributes('style')).toContain('300px')
+  })
+
+  test('block', () => {
+    const wrapper = mount(FImage, {
+      props: { block: true }
+    })
+    expect(wrapper.classes()).toContain('f-image-block')
+  })
+
+  test('fit', () => {
+    const wrapper = mount(FImage, {
+      props: { fit: 'cover' }
+    })
+    expect(wrapper.find('img').classes()).toContain('f-image-cover')
+  })
+
+  test('fit', () => {
+    const wrapper = mount(FImage, {
+      props: { fit: 'cover' }
+    })
+    expect(wrapper.find('img').classes()).toContain('f-image-cover')
+  })
+
+  test('select', () => {
+    const wrapper = mount(FImage, {
+      props: { select: true }
+    })
+    expect(wrapper.find('img').classes()).toContain('f-image-select')
+  })
+
+  test('referrer-policy', () => {
+    const wrapper = mount(FImage, {
+      props: { referrerPolicy: 'no-referrer' }
+    })
+    expect(wrapper.find('img').attributes('referrer-policy')).toContain('no-referrer')
+  })
 })
