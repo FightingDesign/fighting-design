4de47f4a644f840f2cdaabfa0f1d2a4b88760103
15227241216 <1469442737@qq.com>
2022-06-03 12:13:44 +0800

    docs: 更新文档

--- start/src/App.vue
+++ start/src/App.vue
@@ -2,7 +2,6 @@
   <f-button size="large" type="primary">主要按钮</f-button>
   <f-button type="success">成功按钮</f-button>
   <f-button size="small" type="danger">危险按钮</f-button>
-  <f-button size="mini" type="warning">警告按钮</f-button>
 
   <f-image
     width="200px"
