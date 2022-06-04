ea40c85c4c2271cca4688dfef5163e900ea9e666
15227241216 <1469442737@qq.com>
2022-06-04 18:11:53 +0800

    chore: 更新配置项命令

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 export default defineConfig({
-  root: './start',
   plugins: [vue(), vueSetupExtend()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
