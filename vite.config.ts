cdae8b10c3fb5e695ccd8d788c55064a4a2143ba
15227241216 <1469442737@qq.com>
2022-06-04 16:53:28 +0800

    chore: 更新构建流程 优化代码格式

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 export default defineConfig({
@@ -8,17 +7,5 @@ export default defineConfig({
   plugins: [vue(), vueSetupExtend()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
-  // build: {
-  //   // lib: {
-  //   //   entry: resolve(__dirname, 'packages/fighting-components/index.ts'),
-  //   //   name: 'fightingDesign',
-  //   //   formats: ['es'],
-  //   //   fileName: 'index.js'
-  //   // },
-  //   rollupOptions: {
-  //     // 确保外部化处理那些你不想打包进库的依赖
-  //     external: ['vue']
-  //   }
-  // }
+  }
 })
