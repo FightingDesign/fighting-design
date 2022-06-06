f4faa6ef8e915a83c5a46fcd2b6b5858d942f3b7
tyh2001 <1469442737@qq.com>
2022-06-06 09:50:44 +0800

    fix: 修复 divider 组件一些细节

--- packages/fighting-components/divider/src/divider.ts
+++ packages/fighting-components/divider/src/divider.ts
@@ -2,14 +2,14 @@ import type { textAliagn, direction } from "@fighting-design/fighting-type";
 import type { PropType } from "vue";
 
 export const Props = {
-    textAlign: {
-        type: String as PropType<textAliagn>,
-        default: (): textAliagn => 'center',
-        validator: (value: textAliagn): boolean => ['left', 'center', 'right'].includes(value)
-    },
-    direction: {
-        type: String as PropType<direction>,
-        default: (): direction => 'horizontal',
-        validator: (value: direction): boolean => ['horizontal', 'vertical'].includes(value)
-    }
+  textAlign: {
+    type: String as PropType<textAliagn>,
+    default: (): textAliagn => 'center',
+    validator: (value: textAliagn): boolean => (['left', 'center', 'right'] as const).includes(value)
+  },
+  direction: {
+    type: String as PropType<direction>,
+    default: (): direction => 'horizontal',
+    validator: (value: direction): boolean => (['horizontal', 'vertical'] as const).includes(value)
+  }
 } as const;
--- start/src/App.vue
+++ start/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <f-divider
     text-aliagn="x"
-    style="color: darkmagenta; margin: '2rem 0'; border-bottom: 1px dotted darkmagenta"
-    >22</f-divider>
+  >
+    22
+  </f-divider>
 
   <f-divider direction="vertical" />
-  <f-divider direction="vertical" :style="{ margin: '0 4rem', borderLeftColor: 'red' }" />
+  <f-divider
+    direction="vertical"
+    :style="{ margin: '0 4rem', borderLeftColor: 'red' }"
+  />
   <f-divider direction="vertical" />
 </template>
 
