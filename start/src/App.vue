6679843a40e48bc0d152121ce3a3342520bb1a91
“keyie” <570588655@qq.com>
2022-06-09 23:44:50 +0800

    feat:新增tag配置项&更新tag文档

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,18 +1,4 @@
 <template>
-<<<<<<< HEAD
-<f-tag color="#ff0200">
-  <f-icon size="15px" icon="f-icon-close" />
-  浅色模式
-</f-tag>
-<f-tag size="small" type="warning" block :hit="false">深色模式</f-tag>
-  
-
-<f-container>
-    <f-header>Header</f-header>
-    <f-main padding="20px">Main</f-main>
-  </f-container>
-  <f-button>121</f-button>
-=======
   <f-switch v-model="value" icon="f-icon-Daytimemode"></f-switch>
 
   <f-switch
@@ -31,7 +17,6 @@
     disabled
     icon="f-icon-Daytimemode"
   ></f-switch>
->>>>>>> 41cc32ee550acce688d1a81becbb6aad1aad1db1
 </template>
 
 <script lang="ts" setup>
