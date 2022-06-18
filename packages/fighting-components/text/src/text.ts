0a82b7dc7b4a2b1962ccae64ff0f5747a15efbc3
dengqingsong <798833488@qq.com>
2022-06-19 00:32:42 +0800

    feat: 新增 FText 组件

---
+++ packages/fighting-components/text/src/text.ts
@@ -0,0 +1,92 @@
+import { computed } from 'vue'
+import type { PropType, ComputedRef } from 'vue'
+import type { textType } from './interface'
+
+export const Props = {
+  /**
+   * 类型/主题
+   */
+  type: {
+    type: String as PropType<textType>,
+    default: (): textType => 'default',
+    validator: (val: textType): boolean => {
+      return (['primary', 'success', 'danger', 'warning', 'default'] as const).includes(
+        val
+      )
+    }
+  },
+  /**
+   * 文字大小
+   */
+  size: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 字体颜色
+   */
+  color: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 背景颜色
+   */
+  bgColor: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 是否独占一行
+   */
+  block: {
+    type: Boolean,
+    default: (): boolean => false
+  },
+  /**
+   * 字间距
+   */
+  spacing: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 行高
+   */
+  lineHeight: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 缩进量
+   */
+  textIndent: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 是否加粗
+   */
+  bold: {
+    type: Boolean,
+    default: (): boolean => false
+  },
+  /**
+   * 文本修饰
+   */
+  decoration: {
+    type: String,
+    default: (): string => ''
+  },
+  /**
+   * 是否显示下划线，优先级大于文本修饰 decoration
+   */
+  underline: {
+    type: Boolean,
+    default: (): boolean => false
+  }
+} as const
+
+export const Emits = {
+  click: (evt: Event): Event => evt
+} as const
