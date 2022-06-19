6b9c68b5359d72cbbb490a410c50db6148f7fff7
Yn_22638 <1941263063@qq.com>
2022-06-19 10:40:59 +0800

    feat(FLoading): loading组件搭建

---
+++ packages/fighting-components/loading/src/loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Props } from "./type"
+const props = defineProps(Props)
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ packages/fighting-components/loading/src/type.ts
@@ -0,0 +1,14 @@
+export const Props = {
+  loadingText: {
+    type: String,
+    default: '加载中'
+  },
+  loadingTextColor: {
+    type: String,
+    default: '#FFFFFF'
+  },
+  loadingBgColor: {
+    type: String,
+    default: '#000000'
+  }
+}
