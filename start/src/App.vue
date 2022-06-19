9465071c6458b4aa75ab40158428e5bbedde2bee
15227241216 <1469442737@qq.com>
2022-06-19 21:40:36 +0800

    style: 更新色号

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,7 +1,30 @@
 <template>
-  <h1>hello</h1>
+  <f-button type="default">主要按钮</f-button>
+  <br />
+  <f-button :style="{ background: '#456df3' }" type="default"
+    >主要按钮</f-button
+  >
+  <br />
+  <f-button
+    type="default"
+    :style="{ background: changeColor.getLightColor(.54) }"
+  >
+    主要按钮
+  </f-button>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ChangeColor } from '@fighting-design/fighting-utils'
 
-<style scoped></style>
+  const changeColor = new ChangeColor('#f0f0f0')
+
+  console.log(changeColor.getLightColor(.18))
+</script>
+
+<style scoped>
+  div {
+    width: 50px;
+    height: 50px;
+    border: 2px solid black;
+  }
+</style>
