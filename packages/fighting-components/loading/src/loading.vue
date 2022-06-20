59fbe123f906f57703d69bddc67fd4ff39f9f860
matthew <mxy@spacetech.com.cn>
2022-06-20 15:29:42 +0800

    fix(loading): loding文字动画

--- packages/fighting-components/loading/src/loading.vue
+++ packages/fighting-components/loading/src/loading.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="f-loading__mask" v-if="show" :style="{ background: loadingBgColor, opacity: loadingBgOpacity }">
-    <i :class="loadingIcon"></i>
-    <p :style="{ color: loadingTextColor, fontSize: loadingTextSize }">{{ loadingText }}</p>
+  <div class="f-loading__mask" v-if="show"
+    :style="{ background: loadingBgColor, opacity: loadingBgOpacity, color: loadingTextColor, }" @click="onClose">
+    <i :class="loadingIcon"> 我是 icon</i>
+    <div class="f-loading__title" :style="{ fontSize: loadingTextSize }">
+      <p>{{ loadingText }}</p>
+      <span class="f-loading__comma">...</span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Props } from "./props"
+import { Props } from './props'
 const props = defineProps(Props)
+const emits = defineEmits<{
+  (event: 'close'): void
+}>()
+
+const onClose = () => {
+  props.isClose && emits('close')
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
 .f-loading {
   &__mask {
+    position: fixed;
+    top: 0;
+    left: 0;
     z-index: 1011;
     width: 100%;
     height: 100%;
@@ -20,6 +34,32 @@ const props = defineProps(Props)
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  }
+
+  &__title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__comma {
+    position: relative;
+    top: 13px;
+    left: 10px;
+    width: 2.5ch;
+    animation: comma-f4b50242 2s steps(6), effect 0.5s step-end alternate;
+    animation-iteration-count: infinite;
+    white-space: nowrap;
+    overflow: hidden;
+    transform: rotate(180deg);
+    font-size: 23px;
+    letter-spacing: 3px
+  }
+}
+
+@keyframes comma {
+  from {
+    width: 0;
   }
 }
 </style>
--- packages/fighting-components/loading/src/props.ts
+++ packages/fighting-components/loading/src/props.ts
@@ -1,34 +1,39 @@
 export const Props = {
   show: {
     type: Boolean,
-    default: false
+    default: false,
+  },
+  // 是否可以关闭加载中
+  isClose: {
+    type: Boolean,
+    default: false,
   },
   loadingText: {
     type: String,
-    default: '加载中'
+    default: '加载中',
   },
   loadingIcon: {
     type: String,
-    default: 'icon'
+    default: 'icon',
   },
   loadingTextColor: {
     type: String,
-    default: '#FFFFFF'
+    default: 'rgb(25, 137, 250)',
   },
   loadingBgColor: {
     type: String,
-    default: '#000000'
+    default: '#000',
   },
   loadingBgOpacity: {
     type: [String, Number],
-    default: '0.3'
+    default: '0.7',
   },
   loadingTextSize: {
     type: [String, Number],
-    default: '16'
+    default: '16',
   },
   duration: {
     type: [String, Number],
-    default: '0.3'
-  }
+    default: '0.3',
+  },
 }
