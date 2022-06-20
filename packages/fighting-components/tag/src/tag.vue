7b50ce919c40a6a9329835650741387fc898831d
“keyie” <570588655@qq.com>
2022-06-20 21:29:59 +0800

    refactor: 优化代码结构

--- packages/fighting-components/tag/src/tag.vue
+++ packages/fighting-components/tag/src/tag.vue
@@ -3,13 +3,11 @@
     :class="cls"
     :style="sty"
   >
-
     <span class="f-text">
-      <f-icon v-if="leftIcon" size="15px" :icon="leftIcon" />
+      <i v-if="leftIcon" :class="['f-icon', leftIcon]"/>
       <slot />
-      <f-icon v-if="rightIcon" size="15px" :icon="rightIcon" />
-
-      <f-icon v-if="closable" class="f-iolor" size="15px" icon="f-icon-close" @click="handleClose" />
+      <i v-if="rightIcon" :class="['f-icon', rightIcon]"/>
+      <i v-if="closable" :class="['f-icon', 'f-icon-close']" />
     </span>
   </div>
 </template>
@@ -19,29 +17,32 @@
   import type { onClickInterface } from '@fighting-design/fighting-type'
   import { computed } from 'vue';
 
-  const prop = defineProps(Props)
+  const { simple, type, size, block, hit, round, color } = defineProps(Props)
   const emit = defineEmits(Emits)
-  const is_light = prop.simple
+  const ftColor = simple ? '#fff' : color
+  const bgColor = simple ? color : '#fff'
+
   const cls = computed(() => 
     [
       'f-tag',
-      (is_light ? `f-tag-sim-${prop.type}` : `f-tag-${prop.type}`),
+      (simple ? `f-tag-sim-${type}` : `f-tag-${type}`),
       
       {
-        [`f-tag-${prop.size}`]: prop.size,
-        'f-block': prop.block,
-        'f-no-border': !prop.hit,
+        [`f-tag-${size}`]: size,
+        'f-block': block,
+        'f-no-border': !hit,
       }
     ]
   );
 
-  let sty = {
-    borderRadius: prop.round,
-  }
-
-  if(prop.color) {
-    sty = Object.assign(sty, is_light ? { color: prop.color, border: `1px solid ${prop.color}`, backgroundColor: '#fff' } : { backgroundColor: prop.color, color:'#fff', border: `1px solid ${prop.color}` })
-  }
+  const sty = computed(()=> {
+   return {
+     color: ftColor,
+     backgroundColor: bgColor,
+     borderRadius: round, 
+     border: `1px solid ${color}`,
+   }
+  })
 
   const handleClose: onClickInterface = (event: PointerEvent): void => {
     event.stopPropagation()
