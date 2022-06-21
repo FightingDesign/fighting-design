9572ed5320fb39b92893236d4b54f2c2974d7d4e
15227241216 <1469442737@qq.com>
2022-06-21 11:11:55 +0800

    feat: 新增 FTimeLine3334 组件

---
+++ packages/fighting-components/time-line3334/index.ts
@@ -0,0 +1,7 @@
+import FTimeLine3334 from './src/time-line3334.vue'
+
+import { install } from '@fighting-design/fighting-utils'
+
+install(FTimeLine3334, FTimeLine3334.name)
+
+export { FTimeLine3334 }
---
+++ packages/fighting-components/time-line3334/src/time-line3334.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="time-line3334">FTimeLine3334</div>
+</template>
+
+<script lang="ts" setup name="FTimeLine3334">
+  import { Props, Emits } from './time-line3334'
+
+  defineProps(Props)
+  defineEmits(Emits)
+</script>
