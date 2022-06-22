d803cff7710cac92439f47bcb55cf34275df1279
matthew <mxy@spacetech.com.cn>
2022-06-22 17:34:15 +0800

    fix: loading style

--- packages/fighting-components/loading/src/loading.vue
+++ packages/fighting-components/loading/src/loading.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="f-loading__mask" v-if="show"
+  <div class="f-loading-mask" v-if="show"
     :style="{ background: loadingBgColor, opacity: loadingBgOpacity, color: loadingTextColor, }" @click="onClose">
     <i class="f-icon f-loading-animation" :class="loadingIcon" :style="iconStyle" />
-    <p class="f-loading__title" :style="{ fontSize: loadingTextSize }">{{ loadingText }}</p>
+    <p class="f-loading-title" :style="{ fontSize: loadingTextSize }">{{ loadingText }}</p>
   </div>
 </template>
 
@@ -15,26 +15,3 @@ const onClose = () => {
   props.isClose && emits('close')
 }
 </script>
-
-<style lang="scss" scoped>
-.f-loading {
-  &__mask {
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1011;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  &__title {
-    margin: 0;
-    margin-top: 10px;
-  }
-
-}
-</style>
