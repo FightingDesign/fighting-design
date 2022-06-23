4b954a6be5b74f906f14a775d2f35709487c4dce
田同学 <1469442737@qq.com>
2022-06-23 08:42:11 +0800

    fix：修复 text 组件

--- packages/fighting-components/text/src/text.vue
+++ packages/fighting-components/text/src/text.vue
@@ -1,39 +1,51 @@
 <template>
   <span :class="classList" :style="styleList">
-    <slot></slot>
+    <slot />
   </span>
 </template>
 
 <script lang="ts" setup name="FText">
-import { Props } from './text'
-import { computed } from 'vue'
-
-const prop = defineProps(Props)
-
-const classList = computed(() => {
-  const { type, block, bold } = prop
-  return [
-    'f-text',
-    {
-      [`f-text-${type}`]: type,
-      'f-text-block': block,
-      'f-text-bold': bold
+  import { Props } from './text'
+  import { computed } from 'vue'
+  import type { ComputedRef } from 'vue'
+  import type { textStyleInterface } from './interface'
+  const prop = defineProps(Props)
+  const classList: ComputedRef<object | string[]> = computed(
+    (): object | string[] => {
+      const { type, block, bold } = prop
+      return [
+        'f-text',
+        {
+          [`f-text-${type}`]: type,
+          'f-text-block': block,
+          'f-text-bold': bold
+        }
+      ]
     }
-  ]
-})
-
-const styleList = computed(() => {
-  const { size, color, lineHeight, spacing, textIndent, background, decoration, padding } = prop
-  const style = {
-    fontSize: size,
-    color,
-    lineHeight,
-    letterSpacing: spacing,
-    textIndent: textIndent,
-    backgroundColor: background || '#FFF',
-    textDecoration: decoration,
-    padding
-  }
-  return style
-})
+  )
+  const styleList: ComputedRef<textStyleInterface> = computed(
+    (): textStyleInterface => {
+      const {
+        size,
+        color,
+        lineHeight,
+        spacing,
+        textIndent,
+        background,
+        decoration,
+        padding
+      } = prop
+      const style: textStyleInterface = {
+        color,
+        background,
+        textIndent,
+        lineHeight,
+        padding,
+        fontSize: size,
+        letterSpacing: spacing,
+        textDecoration: decoration
+      }
+      return style
+    }
+  )
 </script>
