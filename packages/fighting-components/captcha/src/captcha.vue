29c213e029e46528bb2796db0288cd4dafe35499
15227241216 <1469442737@qq.com>
2022-06-30 20:25:40 +0800

    feat: 实现 captcha 基本效果

--- packages/fighting-components/captcha/src/captcha.vue
+++ packages/fighting-components/captcha/src/captcha.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="f-captcha">
     <div class="f-captcha-verify-box">
-      <img class="f-captcha-verify" src="./prey.png" alt="" />
+      <img class="f-captcha-verify" :src="verifySrc" />
+
       <img
         class="f-captcha-jigsaw"
         src="./prey-item.png"
-        :style="onInput()"
-        alt=""
+        :style="captchaStyleList"
       />
     </div>
 
-    <input v-model="range" type="range" @input="onInput" />
-
     <div class="f-captcha-move">
-      <div ref="captchaItem" class="f-captcha-item">滑动</div>
+      <div ref="captchaItem" class="f-captcha-item">
+        <i :class="['f-icon', moveIcon || 'f-icon-double-arro-right']" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup name="FCaptcha">
   import { Props, Emits } from './captcha'
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, computed } from 'vue'
   import type { Ref } from 'vue'
+  import type { handleInputReturnInterface } from './interface'
 
   defineProps(Props)
   defineEmits(Emits)
@@ -31,24 +32,28 @@
     null as unknown as HTMLDivElement
   )
 
-  const onInput = () => {
+  const captchaStyleList = computed((): handleInputReturnInterface => {
     return {
-      left: ((320 - 60) * range.value) / 100 + 'px'
-    }
-  }
+      left: (260 * range.value) / 100 + 'px'
+    } as const
+  })
 
   onMounted(() => {
-    const node = document.querySelector('.f-captcha-item') as HTMLDialogElement
+    const node: HTMLDivElement = document.querySelector(
+      '.f-captcha-item'
+    ) as HTMLDivElement
 
-    node.addEventListener('mousedown', (evt: MouseEvent) => {
-      const dis = evt.clientX - node.offsetLeft
+    node.addEventListener('mousedown', (evt: MouseEvent): void => {
+      const dis: number = evt.clientX - node.offsetLeft
 
       document.onmousemove = (evt: MouseEvent): void => {
         if (evt.clientX - dis >= 0 && evt.clientX - dis <= 260) {
+          range.value = Math.floor((evt.clientX - dis) / 2.6)
           node.style.left = evt.clientX - dis + 'px'
         }
       }
-      document.onmouseup = () => {
+
+      document.onmouseup = (): void => {
         document.onmousemove = null
       }
     })
--- start/src/App.vue
+++ start/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <f-captcha></f-captcha>
+  <f-captcha
+    verify-src="https://www.hualigs.cn/image/62bd95894189e.jpg"
+    jigsaw-src="https://www.hualigs.cn/image/62bd9588612da.jpg"
+    move-icon="f-icon-Rightarrow"
+  />
 </template>
 
 <script lang="ts" setup></script>
