9a22d0302085991ecf66d538ed66152c60a34815
15227241216 <1469442737@qq.com>
2022-06-30 16:15:46 +0800

    feat: 新增验证拖动功能

--- packages/fighting-components/captcha/src/captcha.vue
+++ packages/fighting-components/captcha/src/captcha.vue
@@ -11,22 +11,46 @@
     </div>
 
     <input v-model="range" type="range" @input="onInput" />
+
+    <div class="f-captcha-move">
+      <div ref="captchaItem" class="f-captcha-item">滑动</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup name="FCaptcha">
   import { Props, Emits } from './captcha'
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
   import type { Ref } from 'vue'
 
   defineProps(Props)
   defineEmits(Emits)
 
   const range: Ref<number> = ref<number>(0)
+  const captchaItem: Ref<HTMLDivElement> = ref<HTMLDivElement>(
+    null as unknown as HTMLDivElement
+  )
 
   const onInput = () => {
     return {
       left: ((320 - 60) * range.value) / 100 + 'px'
     }
   }
+
+  onMounted(() => {
+    const node = document.querySelector('.f-captcha-item') as HTMLDialogElement
+
+    node.addEventListener('mousedown', (evt: MouseEvent) => {
+      const dis = evt.clientX - node.offsetLeft
+
+      document.onmousemove = (evt: MouseEvent): void => {
+        if (evt.clientX - dis >= 0 && evt.clientX - dis <= 260) {
+          node.style.left = evt.clientX - dis + 'px'
+        }
+      }
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    })
+  })
 </script>
