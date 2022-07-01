c8c1fe9629af1abdf7270f37249a43f694e22dba
godwei123 <1374532240@qq.com>
2022-07-01 12:45:23 +0800

    fix：修复space组件

--- packages/fighting-components/space/src/space.vue
+++ packages/fighting-components/space/src/space.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script lang="ts" setup name="FSpace">
-  import { computed, reactive } from 'vue'
+  import { computed, ref } from 'vue'
   import { Props, SpacingType } from './space'
   import { spaceSize } from './interface'
   const prefixClass = 'f-space'
@@ -20,36 +20,37 @@
     return Array.isArray(s)
   }
 
-  const realStyle = reactive({
+  let realStyle = ref({
     ...props.style
   })
 
   const spaceClassObject = computed(() => {
-    const { position, className, wrap, vertical, spacing } = props
+    const { position, className, wrap, vertical, spacing, style } = props
     const isWrap = wrap
 
+    realStyle.value = { ...style }
     let spacingHorizontalType: spaceSize = SpacingType.SPACING_SMALL
     let spacingVerticalType: spaceSize = SpacingType.SPACING_SMALL
     if (isString(spacing)) {
       spacingHorizontalType = spacing
       spacingVerticalType = spacing
     } else if (isNumber(spacing)) {
-      realStyle.rowGap = `${spacing}px`
-      realStyle.columnGap = `${spacing}px`
+      realStyle.value.rowGap = `${spacing}px`
+      realStyle.value.columnGap = `${spacing}px`
     } else if (isArray(spacing)) {
       if (isString(spacing[0])) {
         spacingHorizontalType = spacing[0]
       } else if (isNumber(spacing[0])) {
-        realStyle.columnGap = `${spacing[0]}px`
+        realStyle.value.columnGap = `${spacing[0]}px`
       }
       if (isString(spacing[1])) {
         spacingVerticalType = spacing[1]
       } else if (isNumber(spacing[1])) {
-        realStyle.rowGap = `${spacing[1]}px`
+        realStyle.value.rowGap = `${spacing[1]}px`
       }
     }
     return {
-      [className]: className,
+      [className]: !!className,
       [prefixClass]: true,
       [`${prefixClass}-align-${position}`]: position,
       [`${prefixClass}-vertical`]: vertical,
