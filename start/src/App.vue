e7a48efa0731b5b6872b5fb9ba45c2cfd7b6d85c
15227241216 <1469442737@qq.com>
2022-07-02 23:03:17 +0800

    fix: 修复 start 目录结构

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,5 +1,26 @@
 <template>
- 
+  <f-tag v-show="isShow1" close @on-close="isShow1 = false" type="info">
+    信息
+  </f-tag>
+  <f-tag v-show="isShow2" close @on-close="isShow2 = false" type="primary">
+    普通
+  </f-tag>
+  <f-tag v-show="isShow3" close @on-close="isShow3 = false" type="warning">
+    警告
+  </f-tag>
+  <f-tag v-show="isShow4" close @on-close="isShow4 = false" type="success">
+    成功
+  </f-tag>
+  <f-tag v-show="isShow5" close @on-close="isShow5 = false" type="danger">
+    失败
+  </f-tag>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
+  const isShow1 = ref(true)
+  const isShow2 = ref(true)
+  const isShow3 = ref(true)
+  const isShow4 = ref(true)
+  const isShow5 = ref(true)
+</script>
