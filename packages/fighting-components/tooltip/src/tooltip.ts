f8367af5d58025039d706d9856636f4d2a6c1c92
15227241216 <1469442737@qq.com>
2022-07-06 21:45:46 +0800

    feat: 新增 tooltip 配置项

--- packages/fighting-components/tooltip/src/tooltip.ts
+++ packages/fighting-components/tooltip/src/tooltip.ts
@@ -1,5 +1,23 @@
+import type { PropType } from 'vue'
+import type { positionType } from './interface'
+
 export const Props = {
   content: {
+    type: String,
+    default: (): string => ''
+  },
+  position: {
+    type: String as PropType<positionType>,
+    default: (): positionType => 'bottom',
+    validator: (val: positionType): boolean => {
+      return (['top', 'bottom', 'right', 'left'] as const).includes(val)
+    }
+  },
+  background: {
+    type: String,
+    default: (): string => ''
+  },
+  color: {
     type: String,
     default: (): string => ''
   }
