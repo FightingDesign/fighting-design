c32f3a57d233c2f261c631e5e7863aa8e5f455b7
matthew <mxy@spacetech.com.cn>
2022-07-07 09:23:10 +0800

    fix: app.vue 删除俩行 avatar 标签

--- start/src/App.vue
+++ start/src/App.vue
@@ -2,13 +2,12 @@
   <f-avatar background="red" round fit="fill" :src="src" />
   <f-avatar background="red" round fit="contain" :src="src" />
   <f-avatar background="red" round fit="cover" :src="src" />
-  <f-avatar background="red" round fit="none" :src="src" />
-  <f-avatar background="red" round fit="scale-down" :src="src" />
 </template>
 
 <script lang="ts" setup>
-  const src =
-    'https://fastly.jsdelivr.net/gh/hututu-tech/IMG-gongfeng@main/2022/06/13/62a7273443773.png'
+const src =
+  'https://fastly.jsdelivr.net/gh/hututu-tech/IMG-gongfeng@main/2022/06/13/62a7273443773.png'
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
