72ce4e836194285bca5d15bbba9c4fd9213f78ff
yzj940619 <88603076@qq.com>
2022-07-07 22:50:45 +0800

    feat: 新增page-header组件

--- packages/fighting-components/header/src/header.ts
+++ packages/fighting-components/header/src/header.ts
@@ -1,6 +1,42 @@
 export const Props = {
-  height: {
+  icon: {
     type: String,
-    default: (): string => ''
+    default: (): String => 'f-icon-leftarrow'
+  },
+  iconSize: {
+    type: String,
+    default: (): String => '12px'
+  },
+  backText: {
+    type: String,
+    default: (): String => '返回'
+  },
+  title: {
+    type: String,
+    default: (): String => '标题'
+  },
+  subtitle: {
+    type: String,
+    default: (): String => ''
+  },
+  titleClass: {
+    type: String,
+    default: (): String => ''
+  },
+  titleBold: {
+    type: Boolean,
+    default: (): Boolean => false
+  },
+  titleColor: {
+    type: String,
+    default: (): String => ''
+  },
+  titleCenter: {
+    type: Boolean,
+    default: (): Boolean => false
   }
 } as const
+
+export const Emits = {
+  back: () => true
+}
