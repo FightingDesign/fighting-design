9bdab428e6e05227eb353d369986eb5c93a65f0d
15227241216 <1469442737@qq.com>
2022-07-12 19:39:47 +0800

    fix: 修复 alert 的一些样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <f-alert simple type="default">默认提示信息</f-alert>
+  <f-alert simple type="primary">主要提示信息</f-alert>
+  <f-alert simple type="success">成功提示信息</f-alert>
+  <f-alert simple type="danger">危险提示信息</f-alert>
+  <f-alert simple type="warning">警告提示信息</f-alert>
+</template>
 
 <script setup>
   import { ref } from 'vue'
