eded8166fb83fb43e23f71b642125c080bfa2092
15227241216 <1469442737@qq.com>
2022-07-12 21:39:59 +0800

    style: 更新按钮组件样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <f-button :ripples="false" type="default">默认按钮</f-button>
-  <f-button :ripples="false" type="primary">主要按钮</f-button>
-  <f-button :ripples="false" type="success">成功按钮</f-button>
-  <f-button :ripples="false" type="danger">危险按钮</f-button>
-  <f-button :ripples="false" type="warning">警告按钮</f-button>
+  <f-button type="default">默认按钮</f-button>
+  <f-button type="primary">主要按钮</f-button>
+  <f-button type="success">成功按钮</f-button>
+  <f-button type="danger">危险按钮</f-button>
+  <f-button type="warning">警告按钮</f-button>
   <br />
-  <f-button simple :ripples="false" type="default">默认按钮</f-button>
-  <f-button simple :ripples="false" type="primary">主要按钮</f-button>
-  <f-button simple :ripples="false" type="success">成功按钮</f-button>
-  <f-button simple :ripples="false" type="danger">危险按钮</f-button>
-  <f-button simple :ripples="false" type="warning">警告按钮</f-button>
+  <f-button simple type="default">默认按钮</f-button>
+  <f-button simple type="primary">主要按钮</f-button>
+  <f-button simple type="success">成功按钮</f-button>
+  <f-button simple type="danger">危险按钮</f-button>
+  <f-button simple type="warning">警告按钮</f-button>
   <br />
   <f-button text type="default">默认按钮</f-button>
   <f-button text type="primary">主要按钮</f-button>
