a8d3c8a7e5d033077c4788e7690dc785c141bb61
15227241216 <1469442737@qq.com>
2022-07-17 00:26:30 +0800

    docs: 更新 progress 文档

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <f-progress type="primary" :percentage="50" :height="40" />
+  <f-progress type="success" :percentage="50" />
+  <f-progress type="danger" :percentage="50" />
+  <f-progress type="warning" :percentage="50" />
+</template>
 
 <script lang="ts" setup></script>
 
