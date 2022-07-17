ee27ede90f035c422c9dca5728da2ebc59d2b521
15227241216 <1469442737@qq.com>
2022-07-17 16:01:46 +0800

    docs: 移除冗余代码

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,21 +1,5 @@
-<template>
-  <f-alert type="primary" close @close-end="con">主要提示信息</f-alert>
-  <f-alert type="success" close>成功提示信息</f-alert>
-  <f-alert type="danger" close>危险提示信息</f-alert>
-</template>
+<script lang="ts" setup></script>
 
-<script lang="ts" setup>
-  function con() {
-    console.log(123)
-  }
-</script>
+<template></template>
 
-<style lang="scss" scoped>
-  #box {
-    position: fixed;
-    top: 50px;
-    left: 200px;
-    width: 500px;
-    height: 500px;
-  }
-</style>
+<style scoped></style>
