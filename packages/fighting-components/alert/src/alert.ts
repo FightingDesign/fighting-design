d433f17fa6707d0b4611f99bb8f0b1f21c7e2f37
15227241216 <1469442737@qq.com>
2022-07-17 15:09:14 +0800

    refactor: 删除冗余注释

--- packages/fighting-components/alert/src/alert.ts
+++ packages/fighting-components/alert/src/alert.ts
@@ -2,9 +2,6 @@ import type { PropType } from 'vue'
 import type { alertType } from './interface'
 
 export const Props = {
-  /**
-   * 类型/主题
-   */
   type: {
     type: String as PropType<alertType>,
     default: (): alertType => 'primary',
@@ -14,72 +11,42 @@ export const Props = {
       ).includes(val)
     }
   },
-  /**
-   * 文字大小
-   */
   size: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 是否加粗
-   */
   bold: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 是否居中
-   */
   center: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * ICON
-   */
   icon: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 可关闭
-   */
   close: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 简约
-   */
   simple: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 标题
-   */
   title: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 圆角
-   */
   round: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 背景颜色
-   */
   background: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 字体颜色
-   */
   color: {
     type: String,
     default: (): string => ''
--- packages/fighting-components/text/src/text.ts
+++ packages/fighting-components/text/src/text.ts
@@ -2,9 +2,6 @@ import type { PropType } from 'vue'
 import type { textType } from './interface'
 
 export const Props = {
-  /**
-   * 类型/主题
-   */
   type: {
     type: String as PropType<textType>,
     default: (): textType => 'default',
@@ -14,86 +11,50 @@ export const Props = {
       ).includes(val)
     }
   },
-  /**
-   * 文字大小
-   */
   size: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 字体颜色
-   */
   color: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 背景颜色
-   */
   background: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 是否独占一行
-   */
   block: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 字符间距
-   */
   spacing: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 行高
-   */
   lineHeight: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 缩进量
-   */
   textIndent: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 是否加粗
-   */
   bold: {
     type: Boolean,
     default: (): boolean => false
   },
-  /**
-   * 文本修饰
-   */
   decoration: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 内填充
-   */
   padding: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 自定义宽度
-   */
   width: {
     type: String,
     default: (): string => ''
   },
-  /**
-   * 是否省略
-   */
   ellipsis: {
     type: Boolean,
     default: (): boolean => false
