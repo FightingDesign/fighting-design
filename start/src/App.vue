97a3a7fa72d358e54dc58449880b2815700f1355
tyh2001 <1469442737@qq.com>
2022-07-18 12:58:21 +0800

    feat: 更新按钮组件 focus 样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup></script>
 
 <template>
-  <f-space>
-    <f-button type="default">默认按钮</f-button>
-    <f-button type="primary">主要按钮</f-button>
-    <f-button type="success">成功按钮</f-button>
-    <f-button type="danger">危险按钮</f-button>
-    <f-button type="warning">警告按钮</f-button>
+  <!-- <f-space> -->
+  <f-button type="default">默认按钮</f-button>
+  <f-button type="primary">主要按钮</f-button>
+  <f-button type="success">成功按钮</f-button>
+  <f-button type="danger">危险按钮</f-button>
+  <f-button type="warning">警告按钮</f-button>
 
-    <f-button simple type="default">默认按钮</f-button>
-    <f-button simple type="primary">主要按钮</f-button>
-    <f-button simple type="success">成功按钮</f-button>
-    <f-button simple type="danger">危险按钮</f-button>
-    <f-button simple type="warning">警告按钮</f-button>
+  <f-button simple type="default">默认按钮</f-button>
+  <f-button simple type="primary">主要按钮</f-button>
+  <f-button simple type="success">成功按钮</f-button>
+  <f-button simple type="danger">危险按钮</f-button>
+  <f-button simple type="warning">警告按钮</f-button>
 
-    <f-button circle left-icon="f-icon-camera-fill" type="default" />
-    <f-button circle left-icon="f-icon-good-fill" type="primary" />
-    <f-button circle left-icon="f-icon-discount-fill" type="success" />
-    <f-button circle left-icon="f-icon-Exportservices" type="danger" />
-    <f-button circle left-icon="f-icon-map1" type="warning" />
-  </f-space>
+  <f-button circle left-icon="f-icon-camera-fill" type="default" />
+  <f-button circle left-icon="f-icon-good-fill" type="primary" />
+  <f-button circle left-icon="f-icon-discount-fill" type="success" />
+  <f-button circle left-icon="f-icon-Exportservices" type="danger" />
+  <f-button circle left-icon="f-icon-map1" type="warning" />
+  <!-- </f-space> -->
 </template>
 
 <style scoped></style>
