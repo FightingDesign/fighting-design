0c51deffa45aaff2433eb62ecb853d5ba55152ba
tyh2001 <1469442737@qq.com>
2022-07-20 12:53:19 +0800

    feat: 新增 FAbcde 组件

---
+++ packages/fighting-components/abcde/index.ts
@@ -0,0 +1,7 @@
+import FAbcde from './src/abcde.vue'
+
+import { install } from '@fighting-design/fighting-utils'
+
+install(FAbcde, FAbcde.name)
+
+export { FAbcde }
---
+++ packages/fighting-components/abcde/src/abcde.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup name="FAbcde">
+  import { Props, Emits } from './abcde'
+
+  defineProps(Props)
+  defineEmits(Emits)
+</script>
+
+<template>
+  <div class="f-abcde">FAbcde</div>
+</template>
