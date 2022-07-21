1d6e6cfe2bf253d3d4659acc7baa6178e67a25ca
15227241216 <1469442737@qq.com>
2022-07-21 21:59:22 +0800

    feat: 更新 mask 配置项

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,9 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const visible = ref(false)
+</script>
 
 <template>
-  <f-mack>
+  <f-button type="primary" @click="visible = true">show</f-button>
+  <f-mask v-model:visible="visible">
     <h1>123</h1>
-  </f-mack>
+  </f-mask>
 </template>
 
 <style scoped></style>
