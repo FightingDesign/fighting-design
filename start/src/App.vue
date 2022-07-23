a55d38e0d2b9a2be3d12cced97f681f43762716b
15227241216 <1469442737@qq.com>
2022-07-23 11:48:36 +0800

    chore: 更新打包配置项

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,30 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const onClick = () => {
+    debugger
+  }
+</script>
 
 <template>
-  <f-avatar :size="40" text="你好" src="https://tianyuhao.cn/images/my.jpg" />
-  <f-avatar size="large" src="https://tianyuhao.cn/images/my.jpg" />
-  <f-avatar size="middle" src="https://tianyuhao.cn/images/my.jpg" />
-  <f-avatar size="small" src="https://tianyuhao.cn/images/my.jpg" />
-  <f-avatar size="mini" src="https://tianyuhao.cn/images/my.jpg" />
-
-  <br />
-  <br />
-
-  <f-tag text simple type="default">默认</f-tag>
-  <f-tag text simple type="primary">普通</f-tag>
-  <f-tag text simple type="warning">警告</f-tag>
-  <f-tag text simple type="success">成功</f-tag>
-  <f-tag text simple type="danger">失败</f-tag>
-  <br />
-  <br />
-  <br />
-  <f-space>
-    <f-tag type="default">默认</f-tag>
-    <f-tag type="primary">普通</f-tag>
-    <f-tag type="warning">警告</f-tag>
-    <f-tag type="success">成功</f-tag>
-    <f-tag type="danger">失败</f-tag>
-  </f-space>
+  <f-button ripples type="primary" @click="onClick">普通按钮</f-button>
+  <button @click="onClick">点击</button>
 </template>
 
 <style scoped></style>
