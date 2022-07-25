b8cb416eca4a2dd45790d712188c1de7740452b0
田同学 <1469442737@qq.com>
2022-07-25 23:27:31 +0800

    docs：更新文档配置项

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -1,34 +1,3 @@
-// import { nav } from './utils/nav'
-// import { sidebar } from './utils/sidebar'
-// import { description } from './utils/description'
-// import getBase from '../../src/vitepress/config/baseConfig'
-
-// export default (async () => {
-//   const base = await getBase()
-//   return {
-//     ...base,
-
-//     vite: {
-//       ...base.vite,
-//       build: {
-//         minify: false
-//       }
-//     },
-//     title: 'Fighting Design',
-//     head: [
-//       [
-//         'link',
-//         {
-//           rel: 'icon',
-//           href: 'https://tianyuhao.cn/fighting/imgs/FightingDesign.svg'
-//         }
-//       ]
-//     ],
-//     description,
-//     darkMode: true,
-
-// })()
-
 import { nav } from './utils/nav'
 import { sidebar } from './utils/sidebar'
 import { description } from './utils/description'
