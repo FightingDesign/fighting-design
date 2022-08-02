47707e0f155c40f573f1206ae08012dfc88e25e7
15227241216 <1469442737@qq.com>
2022-08-02 22:48:45 +0800

    docs: 更新文档

--- packages/fighting-components/novel-coronavirus/src/novel-coronavirus.vue
+++ packages/fighting-components/novel-coronavirus/src/novel-coronavirus.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup name="FNovelCoronavirus">
-  import { Props, Emits } from './novel-coronavirus'
   import { ref } from 'vue'
-  import type { Ref } from 'vue'
   import { pastTime } from '@fighting-design/fighting-utils'
+  import type { Ref } from 'vue'
+  import type { ordinaryFunctionInterface as a } from '../../button/src/interface'
 
   const time: Ref<string> = ref<string>('')
+  const start = '2019-12-16' as const
 
-  setInterval(() => {
-    time.value = pastTime('2019-12-16')
-  }, 1000)
+  time.value = pastTime(start)
 
-  defineProps(Props)
-  defineEmits(Emits)
+  const runTime: a = (): void => {
+    setTimeout((): void => {
+      time.value = pastTime(start)
+      runTime()
+    }, 1000)
+  }
+  runTime()
 </script>
 
 <template>
