76574ec144ecb0a5e89d6ac9e573ce4924a0ee57
15227241216 <1469442737@qq.com>
2022-08-11 23:34:04 +0800

    feat: 新增 fighting-icon

---
+++ packages/fighting-icon/index.ts
@@ -0,0 +1,9 @@
+import FIconAiming from './src/f-icon-aiming.vue'
+
+const iconList = {
+  FIconAiming
+}
+
+export { FIconAiming }
+
+export default iconList
--- start/src/App.vue
+++ start/src/App.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
   // import { FMessage } from '../../dist'
+  import { FIconAiming } from '@fighting-design/fighting-icon'
 </script>
 
 <template>
   <f-button href="https://tianyuhao.cn" target="_blank">123</f-button>
+  <FIconAiming fill="red" theme="outline" />
+
+  <aiming theme="outline" size="24" fill="#333" />
 </template>
