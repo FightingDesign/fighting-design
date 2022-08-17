175afc57c0c4eb0865395f4cd72161381e42ff1d
15227241216 <1469442737@qq.com>
2022-08-17 19:55:11 +0800

    fix: 修复按钮组件色号问题

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <f-tooltip
-    content="右边的提示信息"
-    position="right"
-    background="red"
-    font-color="blue"
-  >
-    <f-tag type="warning">信息</f-tag>
-  </f-tooltip>
+  <f-progress :percentage="10" show-text />
+  <f-progress type="danger" :percentage="70" linear height="18px" text-inside />
 </template>
 
 <style scoped></style>
