477f5fdacdbd1da9693f3e57b480d1524990ab15
15227241216 <1469442737@qq.com>
2022-08-18 18:48:03 +0800

    docs: 修复文档配置文件目录

---
+++ docs/docs/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ['vitepress']
+  },
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove()
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+})
