8051fdc98bb70c3c04c9d03cf536c3f88ffa0402
tyh2001 <1469442737@qq.com>
2022-08-22 13:41:53 +0800

    docs: 更新文档

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import './style/vitepress.scss'
-import FightingDesign from '../../../../packages/fighting-design'
+import FightingDesign from '../../../../packages/fighting-design/index'
 import '@fighting-design/fighting-theme'
 
 export default {
--- start/test/index.mjs
+++
@@ -1,1 +0,0 @@
-console.log('我是 ES6 模块')
