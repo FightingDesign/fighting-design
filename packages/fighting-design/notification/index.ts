1ea9f06eef4ffb3b3d039c1651d5b19ebb801bac
tyh2001 <1469442737@qq.com>
2022-09-06 11:32:05 +0800

    fix: 修复 notification 组件导出错误 😅😅😅

--- packages/fighting-design/notification/index.ts
+++ packages/fighting-design/notification/index.ts
@@ -2,6 +2,6 @@ import Notification from './src/method'
 
 import { installFn } from '../_utils'
 
-export const FMessage = /* @__PURE__ */ (() => installFn(Notification, 'FNotification'))()
+export const FNotification = /* @__PURE__ */ (() => installFn(Notification, 'FNotification'))()
 
 export type { FMessageInstance as FNotificationInstance } from '../_interface'
