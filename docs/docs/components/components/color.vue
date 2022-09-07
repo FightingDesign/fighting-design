82d55d09d4ce177fccccc36dc5655d5e698e197f
tyh2001 <1469442737@qq.com>
2022-09-07 17:35:43 +0800

    docs: 更新文档 color 列表 💪💪💪

---
+++ docs/docs/components/components/color.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { ChangeColor } from '../../../../packages/fighting-design/_utils'
+  import type { ComputedRef } from 'vue'
+
+  const colorList: ComputedRef<string[][]> = computed((): string[][] => {
+    const COLOR_LIST = ['#2d5af1', '#52b35e', '#ff0200', '#fcc202'] as const
+
+    const allColorList: string[][] = COLOR_LIST.map(
+      (item: string): string[] => {
+        const series: string[] = []
+        const changeColor: ChangeColor = new ChangeColor(item)
+        for (let i = 0; i < 8; i++) {
+          const background: string = changeColor.getLightColor(
+            i === 0 ? 0 : i / 10 + 0.2
+          )
+          series.push(background)
+        }
+        return series
+      }
+    )
+    return allColorList
+  })
+</script>
+
+<template>
+  <div class="f-color-box">
+    <div v-for="(item, index) in colorList" :key="index" class="f-color-list">
+      <div
+        v-for="(color, i) in item"
+        :key="i"
+        class="f-color-item"
+        :style="{ background: color }"
+      >
+        <f-text>{{ color }}</f-text>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .f-color-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .f-color-item {
+      width: 150px;
+      height: 60px;
+      padding: 10px;
+      box-sizing: border-box;
+      cursor: pointer;
+
+      .f-text {
+        cursor: pointer;
+      }
+    }
+  }
+</style>
