d3926dfcc47167ac5d6862b5abde7d1bcb398d01
tyh2001 <1469442737@qq.com>
2022-09-14 13:53:23 +0800

    fix: 修复调试入口引入 🐫🐫🐫

--- start/main.ts
+++ start/main.ts
@@ -2,17 +2,16 @@ import { createApp } from 'vue'
 import App from './src/App.vue'
 
 // 开发阶段引入
-import { FButton, FImagePreview } from '../packages/fighting-design/index'
+import FightingDesign from '../packages/fighting-design/index'
 import '@fighting-design/fighting-theme'
 
 // 打包阶段引入
 // import FightingDesign from '../dist'
 // import '../dist/dist/index.css'
 
-console.log(FButton)
+console.log(FightingDesign)
 
 createApp(App)
-  .use(FButton)
-  .use(FImagePreview)
+  .use(FightingDesign)
   // .use(FightingDesign)
   .mount('#start')
