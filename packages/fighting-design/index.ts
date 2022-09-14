2cc21f2a38d7656ae35296beb023415eaa61beac
xluoyu <llzz0895@163.com>
2022-09-14 10:31:54 +0800

    fix: 按需引入打包修复

--- packages/fighting-design/index.ts
+++ packages/fighting-design/index.ts
@@ -1,21 +1,4 @@
-import { version } from './package.json'
-import type { App } from 'vue'
-import type { AppInstallInterface } from './_interface'
-import * as components from './components'
+import installer from './defaults'
 export * from './components'
 
-export const install: AppInstallInterface = (app: App): App => {
-  Object.entries(components).forEach(([key, value]): void => {
-    // https://cn.vuejs.org/api/application.html#app-component
-    app.component(key, value)
-  })
-
-  app.config.globalProperties.FMessage = components.FMessage
-  app.config.globalProperties.FNotification = components.FNotification
-  return app
-}
-
-export default {
-  version,
-  install
-}
+export default installer
