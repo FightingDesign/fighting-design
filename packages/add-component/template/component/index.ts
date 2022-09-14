3cac8129391417123444f91619e30ce51467b859
tyh2001 <1469442737@qq.com>
2022-09-14 14:56:52 +0800

    fix: 修复 add-component 模板入口文件 😊😊😊

--- packages/add-component/template/component/index.ts
+++ packages/add-component/template/component/index.ts
@@ -2,8 +2,12 @@ import <%= displayName %> from './src/<%= name %>.vue'
 
 import { install } from '../_utils'
 
-/* @__PURE__ */ install(<%= displayName %>)
-
-export const F<%= displayName %> = <%= displayName %>
+export const F<%= displayName %> = install(<%= displayName %>)
 
 export type F<%= displayName %>Instance = InstanceType<typeof <%= displayName %>>
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    F<%= displayName %>: typeof F<%= displayName %>
+  }
+}
