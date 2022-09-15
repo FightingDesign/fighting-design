d2897c43900283e88af9b8818b517ae6acd2b7d4
15227241216 <1469442737@qq.com>
2022-09-15 19:31:46 +0800

    perf: 优化 expand-card 组件

--- packages/fighting-design/expand-card/src/expand-card.vue
+++ packages/fighting-design/expand-card/src/expand-card.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup name="FExpandCard">
   import { computed, ref } from 'vue'
   import { Props } from './expand-card'
+  import { FImage } from '../../image'
   import type { Ref, ComputedRef } from 'vue'
   import type { classListInterface as d } from '../../_interface'
 
@@ -12,36 +13,29 @@
     currExpandIndex.value = index
   }
 
-  const nodeClassList: ComputedRef<d> = computed((): d => {
+  const activeClass = (index: number): string | void => {
+    if (index !== currExpandIndex.value) return
+    return 'f-expand-card-active'
+  }
+
+  const classList: ComputedRef<d> = computed((): d => {
     const { round } = prop
 
-    return [{ 'f-expand-card-round': round }] as const
+    return ['f-expand-card-item', { 'f-expand-card-round': round }] as const
   })
 </script>
 
 <template>
   <div class="f-expand-card">
     <div
-      v-for="(cardItem, index) in imageList"
+      v-for="(item, index) in imageList"
       :key="index"
-      :style="[
-        prop.round > 0 ? `border-radius:${prop.round}px` : '',
-        `flex-grow:${index === currExpandIndex ? imageList.length : ''}`
-      ]"
-      :class="[
-        'f-expand-card-item',
-        { active: index === currExpandIndex },
-        ...nodeClassList
-      ]"
+      :class="[activeClass(index), ...classList]"
       @click="switchExpandCard(index)"
     >
-      <img :src="cardItem.url" :alt="cardItem.text || ''">
-      <h3
-        v-if="cardItem.text"
-        class="f-expand-card-item__title"
-        :style="{ color }"
-      >
-        {{ cardItem.text }}
+      <f-image :src="item.url" :alt="item.text || ''" />
+      <h3 v-if="item.text" class="f-expand-card-item__title" :style="{ color }">
+        {{ item.text }}
       </h3>
     </div>
   </div>
