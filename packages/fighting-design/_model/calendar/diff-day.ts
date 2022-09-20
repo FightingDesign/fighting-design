f1af1d1cd15ab185e5c05e3f4af830383141867d
tyh2001 <1469442737@qq.com>
2022-09-20 14:07:27 +0800

    fix: 修复日历组件问题

--- packages/fighting-design/_model/calendar/diff-day.ts
+++ packages/fighting-design/_model/calendar/diff-day.ts
@@ -1,25 +1,39 @@
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { dayMonth } from './utils'
 import { lunarCalendar } from '../../_utils/lunar'
+import type { Ref } from 'vue'
 
 /**
  * 计算天数
  */
-export const diffDay = (year: number, month: number) => {
+// export const diffDay = (year: number, month: number) => {
+export const diffDay = (date: Date) => {
+
+  // console.log(year, month)
+  const year: Ref<number> = ref<number>(date.getFullYear())
+  const month: Ref<number> = ref<number>(date.getMonth())
   // 获取当前月份的 1号是周几
-  const firstDayWeek: number = new Date(`${year}/${month + 1}/1`).getDay()
+  const firstDayWeek = ref<number>(new Date(`${year.value}/${month.value + 1}/1`).getDay())
+
+  watch(
+    () => month.value,
+    (newVal: number): void => {
+      console.log(newVal)
+      firstDayWeek.value = new Date(`${year.value}/${newVal}/1`).getDay()
+    }
+  )
 
   // 上个月需要展示的天数
   const lastMonthDay = computed((): [] => {
     // 上个月的天数
-    let lastDays: number = dayMonth(year, month - 1)
+    let lastDays: number = dayMonth(year.value, month.value - 1)
     // 需要展示的上个月信息
     const showLastListResult = []
 
-    for (let i = 0; i < firstDayWeek; i++) {
+    for (let i = 0; i < firstDayWeek.value; i++) {
       const dayList = lunarCalendar.solar2lunar(
-        year,
-        month - 1,
+        year.value,
+        month.value - 1,
         lastDays
       )
       showLastListResult.push(dayList)
@@ -30,22 +44,22 @@ export const diffDay = (year: number, month: number) => {
   })
 
   // 下个月需要展示的天数
-  const nestMonthDay = computed(() => {
+  const nextMonthDay = computed(() => {
     // 获取当前月份的时间
-    const thisMonthDay: number = dayMonth(year, month) + firstDayWeek
+    const thisMonthDay: number = dayMonth(year.value, month.value) + firstDayWeek.value
     // 下个月需要展示的天数
-    const nestShowDay: number = 7 - thisMonthDay % 7
+    const nextShowDay: number = 7 - thisMonthDay % 7
 
-    if (!nestShowDay) {
+    if (!nextShowDay) {
       return []
     }
 
     const showNextListResult = []
 
-    for (let i = 0; i < nestShowDay; i++) {
+    for (let i = 0; i < nextShowDay; i++) {
       const dayList = lunarCalendar.solar2lunar(
-        year,
-        month + 2,
+        year.value,
+        month.value + 2,
         i + 1
       )
       showNextListResult.push(dayList)
@@ -56,12 +70,34 @@ export const diffDay = (year: number, month: number) => {
 
   // 获取当前月份多少天
   const currentMonthDay = computed(() => {
-    return dayMonth(year, month)
+    return dayMonth(year.value, month.value)
   })
+
+  // 点击上个月切换按钮
+  const changeLastMonth = (): void => {
+    if (month.value > 0) {
+      month.value--
+      return
+    }
+    year.value--
+    month.value = 11
+  }
+
+  // 点击下个月切换按钮
+  const changeNextMonth = (): void => {
+    if (month.value < 11) {
+      month.value++
+      return
+    }
+    year.value++
+    month.value = 0
+  }
 
   return {
     lastMonthDay,
-    nestMonthDay,
-    currentMonthDay
+    nextMonthDay,
+    currentMonthDay,
+    changeLastMonth,
+    changeNextMonth
   }
 }
--- packages/fighting-design/calendar/src/calendar.vue
+++ packages/fighting-design/calendar/src/calendar.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup name="FCalendar">
   import { Props } from './calendar'
-  import { ref, computed, watchEffect } from 'vue'
+  import { ref, computed, watchEffect, watch } from 'vue'
   import { FIcon } from '../../index'
   import { lunarCalendar } from '../../_utils'
   import type { Ref, ComputedRef } from 'vue'
@@ -11,52 +11,128 @@
     getLunarInterface as f,
     handleClickInterface as g
   } from './interface'
-  import type {
-    solar2lunarReturnInterface as h
-    // classListInterface as i
-  } from '../../_interface'
+  import type { solar2lunarReturnInterface as h } from '../../_interface'
   import { WEEK_DATA } from '../../_model/calendar/data'
-  import { diffDay } from '../../_model/calendar/diff-day'
+  import { dayMonth } from '../../_model/calendar/utils'
 
   const prop = defineProps(Props)
 
-  const getYear: Ref<number> = ref<number>(prop.date.getFullYear())
-  const getMonth: Ref<number> = ref<number>(prop.date.getMonth())
-  const getDate: Ref<number> = ref<number>(prop.date.getDate())
+  const year: Ref<number> = ref<number>(prop.date.getFullYear())
+  const month: Ref<number> = ref<number>(prop.date.getMonth())
+  const date: Ref<number> = ref<number>(prop.date.getDate())
   const detailDay: Ref<h> = ref<h>(null as unknown as h)
+  // 获取当前月份的 1号是周几
+  const firstDayWeek = ref(
+    new Date(`${year.value}/${month.value + 1}/1`).getDay()
+  )
 
-  const loadDiffDay = diffDay(getYear.value, getMonth.value)
+  watch(
+    () => month.value,
+    (newVal: number): void => {
+      firstDayWeek.value = new Date(`${year.value}/${newVal + 1}/1`).getDay()
+    }
+  )
 
-  const { lastMonthDay, nestMonthDay, currentMonthDay } = loadDiffDay
+  // 上个月需要展示的天数
+  const lastMonthDay = computed((): [] => {
+    // 上个月的天数
+    let lastDays: number = dayMonth(year.value, month.value - 1)
+    // 需要展示的上个月信息
+    const showLastListResult = []
+
+    for (let i = 0; i < firstDayWeek.value; i++) {
+      const dayList = lunarCalendar.solar2lunar(
+        year.value,
+        month.value,
+        lastDays
+      )
+      showLastListResult.push(dayList)
+      lastDays--
+    }
+
+    return showLastListResult.reverse()
+  })
+
+  // 下个月需要展示的天数
+  const nextMonthDay = computed(() => {
+    // 获取当前月份的时间
+    const thisMonthDay: number =
+      dayMonth(year.value, month.value) + firstDayWeek.value
+    // 下个月需要展示的天数
+    const nextShowDay: number = 7 - (thisMonthDay % 7)
+
+    if (!nextShowDay) {
+      return []
+    }
+
+    const showNextListResult = []
+
+    for (let i = 0; i < nextShowDay; i++) {
+      const dayList = lunarCalendar.solar2lunar(
+        year.value,
+        month.value + 2,
+        i + 1
+      )
+      showNextListResult.push(dayList)
+    }
+
+    return showNextListResult
+  })
+
+  // 获取当前月份多少天
+  const currentMonthDay = computed(() => {
+    return dayMonth(year.value, month.value)
+  })
+
+  // 点击上个月切换按钮
+  const changeLastMonth = (): void => {
+    if (month.value > 0) {
+      month.value--
+      return
+    }
+    year.value--
+    month.value = 11
+  }
+
+  // 点击下个月切换按钮
+  const changeNextMonth = (): void => {
+    if (month.value < 11) {
+      month.value++
+      return
+    }
+    year.value++
+    month.value = 0
+  }
+
+  // const loadDiffDay = diffDay(getYear.value, getMonth.value)
+  // const loadDiffDay = diffDay(prop.date)
+
+  // const {
+  //   lastMonthDay,
+  //   nextMonthDay,
+  //   currentMonthDay,
+  //   changeLastMonth,
+  //   changeNextMonth
+  // } = loadDiffDay
 
   // 当前日期高亮显示
   const mowDataClassList: c = (data: number): string => {
-    return data === getDate.value ? 'f-calendar-day-today' : ''
+    return data === date.value ? 'f-calendar-day-today' : ''
   }
 
   // 点击操作栏
   const optionClick: d = (evt: MouseEvent): void => {
     const option = {
       'f-icon f-icon-arrow-left': (): void => {
-        if (getMonth.value > 0) {
-          getMonth.value--
-          return
-        }
-        getYear.value--
-        getMonth.value = 11
+        changeLastMonth()
       },
       'f-icon f-icon-column1': (): void => {
-        getMonth.value = prop.date.getMonth()
-        getYear.value = prop.date.getFullYear()
-        getDate.value = prop.date.getDate()
+        month.value = prop.date.getMonth()
+        year.value = prop.date.getFullYear()
+        date.value = prop.date.getDate()
       },
       'f-icon f-icon-arrow-right': (): void => {
-        if (getMonth.value < 11) {
-          getMonth.value++
-          return
-        }
-        getYear.value++
-        getMonth.value = 0
+        changeNextMonth()
       }
     }
 
@@ -73,16 +149,16 @@
 
   // 当前时间
   const nowTime: ComputedRef<string> = computed((): string => {
-    return `${getYear.value}年 ${addPrefix(getMonth.value + 1)}月 ${addPrefix(
-      getDate.value
+    return `${year.value}年 ${addPrefix(month.value + 1)}月 ${addPrefix(
+      date.value
     )}日`
   })
 
   // 农历
   const getLunar: f = (day: number): h => {
     const lunarDate = lunarCalendar.solar2lunar(
-      getYear.value,
-      getMonth.value + 1,
+      year.value,
+      month.value + 1,
       day
     )
     return lunarDate as h
@@ -91,16 +167,16 @@
   // 修改下面页脚内容
   watchEffect((): void => {
     const lunarDate = lunarCalendar.solar2lunar(
-      getYear.value,
-      getMonth.value + 1,
-      getDate.value
+      year.value,
+      month.value + 1,
+      date.value
     )
     detailDay.value = lunarDate as h
   })
 
   // 点击对每一天
   const handleClick: g = (day: number): void => {
-    getDate.value = day
+    date.value = day
   }
 </script>
 
@@ -158,7 +234,7 @@
       </li>
 
       <li
-        v-for="day in nestMonthDay"
+        v-for="day in nextMonthDay"
         :key="day"
         :class="['f-calendar-day-li', 'f-calendar-day-li-last']"
       >
