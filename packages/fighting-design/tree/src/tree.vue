a1c70bc759590dd62a86abab14f001e349a3b4a4
15227241216 <1469442737@qq.com>
2022-09-24 23:34:01 +0800

    style(tree): 移除 tree 冗余代码 🐝🐝🐝

--- packages/fighting-design/tree/src/tree.vue
+++ packages/fighting-design/tree/src/tree.vue
@@ -4,8 +4,8 @@
   import { treeToFlat, treeAddLevel } from '../../_utils'
   import type { Ref } from 'vue'
   import type {
-    treeAddLevelReturnInterface as a,
-    dataInterface
+    treeAddLevelReturnInterface as a
+    // dataInterface
   } from '../../_interface'
 
   const prop = defineProps(Props)
@@ -15,28 +15,28 @@
 
   // console.log(treeAddLevel(prop.data))
 
-  const openedTree = (tree: dataInterface[]) => {
-    console.log(tree)
+  // const openedTree = (tree: dataInterface[]) => {
+  //   console.log(tree)
 
-    return tree.reduce((prev: a[], item: dataInterface): a[] => {
-      // console.log(prev)
-      // console.log(item)
+  //   return tree.reduce((prev: a[], item: dataInterface): a[] => {
+  //     // console.log(prev)
+  //     // console.log(item)
 
-      // 第一次循环，prev的值，默认为数组的第一项，
-      // 而cur的值为数组的第二项，也就是第一次循环，
-      // 第一次循环返回的结果回传给下一次循环中方法的第一个参数，
-      // 也就是说、第二次循环方法中prev的值，是第一次循环方法返回的结果
+  //     // 第一次循环，prev的值，默认为数组的第一项，
+  //     // 而cur的值为数组的第二项，也就是第一次循环，
+  //     // 第一次循环返回的结果回传给下一次循环中方法的第一个参数，
+  //     // 也就是说、第二次循环方法中prev的值，是第一次循环方法返回的结果
 
-      // console.log('上个元素', prev)
-      // console.log('当前元素', item)
+  //     // console.log('上个元素', prev)
+  //     // console.log('当前元素', item)
 
-      return item.show
-        ? prev.concat(item, openedTree(item.children))
-        : prev.concat({ ...item, children: null })
-    }, [])
-  }
+  //     return item.show
+  //       ? prev.concat(item, openedTree(item.children))
+  //       : prev.concat({ ...item, children: null })
+  //   }, [])
+  // }
 
-  console.log(openedTree(treeAddLevel(prop.data)))
+  // console.log(openedTree(treeAddLevel(prop.data)))
 
   const handleClick = (item, index): void => {
     console.log(item, index)
