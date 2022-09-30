65e0be7ba9e772cb4e1a0e914c172dc343f2b589
tyh2001 <1469442737@qq.com>
2022-09-30 20:03:38 +0800

    chore: 更新配置文件 更新 start

--- packages/fighting-design/alert/src/interface.d.ts
+++ packages/fighting-design/alert/src/interface.d.ts
@@ -1,11 +1,17 @@
-// import type Alert from './alert.vue'
-// import type { Props } from './alert'
-// import type { ExtractPropTypes } from 'vue'
+import type FAlert from './alert.vue'
+import type { Props } from './alert'
+import type { ExtractPropTypes } from 'vue'
 
 export type AlertType = 'primary' | 'success' | 'danger' | 'warning' | 'default'
 
 export type AlertOverflowType = 'hidden' | ''
 
-// export type AlertInstance = InstanceType<typeof Alert>
+export type AlertInstance = InstanceType<typeof FAlert>
 
-// export type AlertPropsType = ExtractPropTypes<typeof Props>
+export type AlertPropsType = ExtractPropTypes<typeof Props>
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    FAlert: typeof FAlert
+  }
+}
--- start/src/main.ts
+++ start/src/main.ts
@@ -6,11 +6,11 @@ import App from './App.vue'
 // new VConsole()
 
 // 开发阶段引入
-import FightingDesign from '../../packages/fighting-design/index'
+// import FightingDesign from '../../packages/fighting-design/index'
 import '@fighting-design/fighting-theme'
 
 // 打包阶段引入
-// import FightingDesign from '../dist'
+import FightingDesign from '../../dist'
 // import '../dist/dist/index.css'
 
 console.log(FightingDesign)
