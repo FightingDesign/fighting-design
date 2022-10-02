b216370f76c9a223079526a0fde0225ef392cda3
tyh2001 <1469442737@qq.com>
2022-10-02 11:21:52 +0800

    chore: playground 演练场目录迁移至此 🌈🌈🌈

---
+++ packages/fighting-playground/src/utils/index.ts
@@ -0,0 +1,20 @@
+import { unzlibSync, strToU8, strFromU8 } from 'fflate'
+
+// prefer old unicode hacks for backward compatibility
+// https://base64.guru/developers/javascript/examples/unicode-strings
+export const utoa = (data: string): string => {
+  return btoa(unescape(encodeURIComponent(data)))
+}
+
+export const atou = (base64: string): string => {
+  const binary: string = atob(base64)
+  if (binary.startsWith('\x78\xDA')) {
+    const buffer: Uint8Array = strToU8(binary, true)
+    const unzipped: Uint8Array = unzlibSync(buffer)
+    return strFromU8(unzipped)
+  }
+
+  // old unicode hacks for backward compatibility
+  // https://base64.guru/developers/javascript/examples/unicode-strings
+  return decodeURIComponent(escape(binary))
+}
---
+++ packages/fighting-playground/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  server: {
+    port: 7171
+  }
+})
