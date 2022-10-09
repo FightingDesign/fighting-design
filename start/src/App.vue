8e043dea80b589d7b8cb05dfe596c2cd7bce7f75
tyh2001 <1469442737@qq.com>
2022-10-09 20:05:33 +0800

    fix: 修复复选框禁用状态样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,23 +1,21 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
 
-  const option2 = ref(['备选项1'])
-
-  const radio1 = ref('1')
+  const option3 = ref([])
 </script>
 
 <template>
-  <f-checkbox-group v-model="option2">
-    <f-checkbox label="备选项1" />
-    <f-checkbox label="备选项2" />
-    <f-checkbox label="备选项3" />
+  <f-checkbox label="选项一" disabled />
+  <br />
+  <f-checkbox-group v-model="option3" disabled>
+    <f-checkbox label="选项一" />
+    <f-checkbox label="选项二" />
+    <f-checkbox label="选项三" />
   </f-checkbox-group>
-
-  <!-- <br /> -->
-
-  <f-radio-group v-model="radio1">
-    <f-radio label="1">备选项1</f-radio>
-    <f-radio label="2">备选项2</f-radio>
-    <f-radio label="3">备选项3</f-radio>
-  </f-radio-group>
+  <br />
+  <f-checkbox-group v-model="option3">
+    <f-checkbox label="选项一" />
+    <f-checkbox label="选项二" disabled />
+    <f-checkbox label="选项三" />
+  </f-checkbox-group>
 </template>
