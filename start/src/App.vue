a41452967402a892fe682145874fa891a56b2ef9
tyh2001 <1469442737@qq.com>
2022-10-11 21:42:01 +0800

    chore: 更新 start 🥘🥘🥘

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,27 +1,5 @@
-<script setup>
-  import { ref } from 'vue'
+<script lang="ts" setup></script>
 
-  const visible1 = ref(false)
-  const direction = ref('right')
+<template></template>
 
-  const onShow = (dir) => {
-    direction.value = dir
-    visible1.value = true
-  }
-</script>
-
-<template>
-  <f-button type="primary" @click="onShow('left')">从左往右开</f-button>
-  <f-button type="primary" @click="onShow('right')">从右往左开</f-button>
-  <f-button type="primary" @click="onShow('top')">从上往下开</f-button>
-  <f-button type="primary" @click="onShow('bottom')">从下往上开</f-button>
-
-  <f-drawer
-    v-model:visible="visible1"
-    title="Title"
-    :direction="direction"
-    :show-header="false"
-  >
-    fighting-design
-  </f-drawer>
-</template>
+<style lang="scss" scoped></style>
