eff7d6355e299c1fb4a8153fe0a8b1d73402bbee
tyh2001 <1469442737@qq.com>
2022-10-11 08:41:46 +0800

    fix: 修复 radio 和 checkbox 样式细节

--- start/src/App.vue
+++ start/src/App.vue
@@ -2,23 +2,28 @@
   import { ref } from 'vue'
 
   const option3 = ref(['选项一'])
+
   const radio6 = ref('1')
 </script>
 
 <template>
-  <f-radio-group v-model="radio6" :column-gap="120">
+  <f-radio-group v-model="radio6" border>
     <f-radio label="1">备选项1</f-radio>
     <f-radio label="2">备选项2</f-radio>
     <f-radio label="3">备选项3</f-radio>
   </f-radio-group>
+
   <br />
+
   <f-checkbox-group v-model="option3" border>
-    <f-checkbox label="备选项1" />
-    <f-checkbox label="备选项2" />
-    <f-checkbox label="备选项3" />
+    <f-checkbox label="选项一" />
+    <f-checkbox label="选项二" />
+    <f-checkbox label="选项三" />
   </f-checkbox-group>
+
   <br />
-  <f-checkbox-group v-model="option3">
+
+  <f-checkbox-group v-model="option3" border>
     <f-checkbox label="选项一" />
     <f-checkbox label="选项二" disabled />
     <f-checkbox label="选项三" />
