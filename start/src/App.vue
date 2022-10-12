9e96092547d31cb66e93509af2061e47b095307d
tyh2001 <1469442737@qq.com>
2022-10-12 14:54:18 +0800

    fix: 修复 dialog 动画样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,31 +1,5 @@
-<script setup lang="ts">
-  import { ref } from 'vue'
+<script lang="ts" setup></script>
 
-  const visible1 = ref(true)
-</script>
+<template></template>
 
-<template>
-  <f-button type="primary" @click="visible1 = true">打开</f-button>
-
-  <f-dialog v-model:visible="visible1" title="Title">
-    fighting-design
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <h1>121</h1>
-    <template #footer> 12121 </template>
-  </f-dialog>
-</template>
+<style lang="scss" scoped></style>
