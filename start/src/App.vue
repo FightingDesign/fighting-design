800ae6399d8b75667218586aebd86f55c6a54efb
tyh2001 <1469442737@qq.com>
2022-10-14 19:14:34 +0800

    feat: 更新 popup 组件动画效果

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
 
-  const show = ref(true)
+  const show = ref(false)
+
+  const visible2 = ref(false)
 
   const onOpen = (): void => console.log('打开了')
   const onOpenEnd = (): void => console.log('打开完成')
@@ -10,7 +12,13 @@
 </script>
 
 <template>
-  <f-button @click="show = true">打开</f-button>
+  <f-button type="primary" @click="visible2 = true">打开drawer</f-button>
+
+  <f-drawer v-model:visible="visible2" title="Title" :show-header="false">
+    fighting-design
+  </f-drawer>
+
+  <f-button @click="show = true">打开 popup</f-button>
 
   <f-popup
     v-model:visible="show"
@@ -20,7 +28,7 @@
     @close="onClose"
     @close-end="onCloseEnd"
   >
-    <p>这是一段文字</p>
+    <p>这是一段文字这是一段文字这是一段文字这是一段文字</p>
     <p>这是一段文字</p>
     <p>这是一段文字</p>
     <p>这是一段文字</p>
