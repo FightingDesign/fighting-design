5124b825d32f9b99288580d1244221ff1e00259c
tyh2001 <1469442737@qq.com>
2022-10-16 01:09:00 +0800

    chore: 更新 svg 配置项 🌻🌻🌻

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,7 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { FSignIn } from '@fighting-design/fighting-icon'
+</script>
 
 <template>
-  <f-svg-icon></f-svg-icon>
+  <f-svg-icon>
+    <f-sign-in />
+  </f-svg-icon>
 </template>
 
 <style lang="scss" scoped></style>
