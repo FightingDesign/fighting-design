a33e8c5758754aab83ec8f3a492622bb026ef97d
tyh2001 <1469442737@qq.com>
2022-10-17 08:33:02 +0800

    chore: 更新 svg 打包配置文件 🤸🤸🤸

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,37 +1,10 @@
 <script lang="ts" setup>
-  import Svg from '@fighting-design/fighting-icon'
+  // import { FIconAppsMinus } from '@fighting-design/fighting-icon'
+  import { FIconAppsMinus } from '../../dist-svg'
+
+  console.log(FIconAppsMinus)
 </script>
 
-<template>
-  <div class="icon-box">
-    <div v-for="(key, i) of Svg" :key="i" class="icon-list">
-      <f-svg-icon :size="30">
-        <component :is="key" />
-      </f-svg-icon>
-      <span class="icon-name">{{ i }}</span>
-    </div>
-  </div>
-</template>
+<template></template>
 
-<style lang="scss" scoped>
-  .icon-box {
-    width: 100%;
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(7, minmax(0, 1fr));
-
-    .icon-list {
-      height: 90px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-    }
-
-    .icon-name {
-      font-size: 13px;
-      color: #333;
-      margin-top: 10px;
-    }
-  }
-</style>
+<style lang="scss" scoped></style>
