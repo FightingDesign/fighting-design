0009e8ab368a53c9b7934386e323c2e97c20ce7e
tyh2001 <1469442737@qq.com>
2022-10-19 18:16:11 +0800

    fix: 更新 add-components 构建 🌈🌈🌈

--- packages/add-component/template/component/index.ts
+++ packages/add-component/template/component/index.ts
@@ -4,12 +4,4 @@ import { install } from '../_utils'
 
 export const F<%= displayName %> = install(<%= displayName %>)
 
-export type <%= displayName %>Instance = InstanceType<typeof <%= displayName %>>
-
-export type { <%= displayName %>PropsType } from './src/<%= name %>'
-
-declare module 'vue' {
-  export interface GlobalComponents {
-    F<%= displayName %>: typeof F<%= displayName %>
-  }
-}
+export default <%= displayName %>
