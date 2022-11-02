83a82a3a0c8c6d9f05520aa96be6082bf3afd004
tyh2001 <1469442737@qq.com>
2022-11-02 17:46:48 +0800

    feat: 实现 web components 组件升级

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup></script>
 
 <template>
-  <a-button>按钮</a-button>
-  <a-button-ce>按钮</a-button-ce>
+  <f-button type="default">默认按钮</f-button>
+  <f-button type="primary">主要按钮</f-button>
+  <f-button type="success">成功按钮</f-button>
+  <f-button type="danger">危险按钮</f-button>
+  <f-button type="warning">警告按钮</f-button>
+
+  <f-button simple type="default">默认按钮</f-button>
+  <f-button simple type="primary">主要按钮</f-button>
+  <f-button simple type="success">成功按钮</f-button>
+  <f-button simple type="danger">危险按钮</f-button>
+  <f-button simple type="warning">警告按钮</f-button>
 </template>
 
 <style lang="scss" scoped></style>
