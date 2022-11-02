f4b467712cce1830e42739e6a470a7c12f05dc63
xluoyu <llzz0895@163.com>
2022-11-02 10:03:25 +0800

    fix eslint

---
+++ packages/fighting-resolver/README.md
@@ -0,0 +1,3 @@
+# fighting-resolver
+
+用于 [unplugin-vue-components](https://github.com/antfu/unplugin-vue-components) 的导入配置
---
+++ packages/fighting-resolver/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import type { UserConfigExport } from 'vite'
+import dts from 'vite-plugin-dts' // https://github.com/qmhc/vite-plugin-dts
+
+export default (): UserConfigExport => {
+  return {
+    build: {
+      outDir: resolve(__dirname, '../../dist/resolver'),
+      lib: {
+        entry: resolve(__dirname, 'index.ts'), // 打包入口
+        formats: ['es'] // 仅需打包es模块即可
+      }
+    },
+    plugins: [dts()]
+  } as UserConfigExport
+}
