a1cd7b6fa7f28f79a0bef06fc19dc1fd757401af
tyh2001 <1469442737@qq.com>
2022-11-05 13:55:22 +0800

    style: 优化 watermark 组件类型和规范

--- packages/fighting-design/watermark/src/interface.d.ts
+++ packages/fighting-design/watermark/src/interface.d.ts
@@ -1,7 +1,1 @@
-import type { CSSProperties } from 'vue'
-
 export type { WatermarkPropsType } from './props'
-
-export interface WatermarkCSSPropertiesInterface {
-  (): CSSProperties
-}
--- packages/fighting-design/watermark/src/watermark.vue
+++ packages/fighting-design/watermark/src/watermark.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup name="FWatermark">
   import { Props } from './props'
   import { createWatermark } from '../../_utils'
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, computed } from 'vue'
   import { useFilterProps } from '../../_hooks'
-  import type { Ref, CSSProperties } from 'vue'
+  import type { Ref, CSSProperties, ComputedRef } from 'vue'
   import type { WatermarkPropsType } from './props'
-  import type { WatermarkCSSPropertiesInterface } from './interface'
   import type { CreateWatermarkPropsInterface } from '../../_utils/create-watermark/interface'
 
   const prop: WatermarkPropsType = defineProps(Props)
@@ -14,30 +13,38 @@
     null as unknown as CSSProperties
   )
 
-  const baseWatermark: WatermarkCSSPropertiesInterface = (): CSSProperties => {
-    const needProps: CreateWatermarkPropsInterface = useFilterProps<
-      WatermarkPropsType,
-      CreateWatermarkPropsInterface
-    >(prop, ['content', 'width', 'height', 'fontSize', 'fontColor'])
+  // 文字水印
+  const baseWatermark: ComputedRef<CSSProperties> = computed(
+    (): CSSProperties => {
+      const needProps: CreateWatermarkPropsInterface = useFilterProps<
+        WatermarkPropsType,
+        CreateWatermarkPropsInterface
+      >(prop, ['content', 'width', 'height', 'fontSize', 'fontColor'])
 
-    const watermark: string = createWatermark(needProps)
+      const watermark: string = createWatermark(needProps)
 
-    return {
-      backgroundImage: `url(${watermark})`
-    } as CSSProperties
-  }
+      return {
+        backgroundImage: `url(${watermark})`
+      } as CSSProperties
+    }
+  )
 
-  const imageWatermark: WatermarkCSSPropertiesInterface = (): CSSProperties => {
-    const { image, width, height } = prop
+  // 图片水印
+  const imageWatermark: ComputedRef<CSSProperties> = computed(
+    (): CSSProperties => {
+      const { image, width, height } = prop
 
-    return {
-      backgroundImage: `url(${image})`,
-      backgroundSize: `${width}px ${height}px`
-    } as CSSProperties
-  }
+      return {
+        backgroundImage: `url(${image})`,
+        backgroundSize: `${width}px ${height}px`
+      } as CSSProperties
+    }
+  )
 
   onMounted((): void => {
-    watermarkStyleList.value = prop.image ? imageWatermark() : baseWatermark()
+    watermarkStyleList.value = prop.image
+      ? imageWatermark.value
+      : baseWatermark.value
   })
 </script>
 
