8ecf937a851f920325f16c6bdd91466ae6c0fcc3
tyh2001 <1469442737@qq.com>
2022-11-07 17:45:14 +0800

    fix: 修复稳定引入错误 🎁🎁🎁

--- docs/docs/.vitepress/config/plugins.ts
+++ docs/docs/.vitepress/config/plugins.ts
@@ -1,6 +1,6 @@
-import MarkdownIt from 'markdown-it'
 // import MarkdownItContainer from 'markdown-it-container'
 import TableContainer from './table'
+import type MarkdownIt from 'markdown-it'
 // import type { Token } from 'markdown-it'
 
 // https://markdown-it.docschina.org/#%E7%94%A8%E6%B3%95%E7%A4%BA%E4%BE%8B
