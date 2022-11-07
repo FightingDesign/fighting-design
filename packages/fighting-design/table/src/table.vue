e1f509c1fcdd796d6e3c4262b7a6763a560a648e
tyh2001 <1469442737@qq.com>
2022-11-07 17:19:14 +0800

    feat: 新增 table 新属性 🎯🎯🎯

--- packages/fighting-design/table/src/table.vue
+++ packages/fighting-design/table/src/table.vue
@@ -2,28 +2,10 @@
   import { Props } from './props'
   import { computed } from 'vue'
   import { sizeChange } from '../../_utils'
-  import type { ClassListInterface } from '../../_interface'
   import type { ComputedRef, CSSProperties } from 'vue'
   import type { TablePropsType } from './interface'
 
   const prop: TablePropsType = defineProps(Props)
-
-  /**
-   * 类名列表
-   */
-  const classList: ComputedRef<ClassListInterface> = computed(
-    (): ClassListInterface => {
-      const { border, zebra } = prop
-
-      return [
-        'f-table__container',
-        {
-          'f-table__border': border,
-          'f-table__zebra': zebra
-        }
-      ] as const
-    }
-  )
 
   /**
    * 样式列表
@@ -40,48 +22,48 @@
 
 <template>
   <div class="f-table" :style="styleList">
-    <div class="f-table__content">
-      <div v-if="height" class="f-table__header">
+    <div class="f-table__container">
+      <!-- 头部 -->
+      <header v-if="height" class="f-table__header">
         <table>
           <colgroup>
             <col width="150" />
             <col width="190" />
             <col width="240" />
           </colgroup>
-          <tr>
-            <th v-if="num">序号</th>
-            <th v-for="(column, index) in columns" :key="index">
-              {{ column.title }}
-            </th>
-          </tr>
+          <thead>
+            <tr>
+              <th v-if="num">序号</th>
+              <th v-for="(column, index) in columns" :key="index">
+                {{ column.title }}
+              </th>
+            </tr>
+          </thead>
         </table>
-      </div>
-    </div>
-
-    <table border="0" :class="classList">
-      <!-- 头部 -->
-      <thead class="f-table__thead" :align="align">
-        <tr class="f-table__tr">
-          <th v-if="num" class="f-table__th">序号</th>
-          <th
-            v-for="(column, index) in columns"
-            :key="index"
-            class="f-table__th"
-          >
-            {{ column.title }}
-          </th>
-        </tr>
-      </thead>
+      </header>
 
       <!-- 身体 -->
-      <tbody class="f-table__tbody" :align="align">
-        <tr v-for="(dataItem, m) in data" :key="m" class="f-table__tr">
-          <td v-if="num" class="f-table__td">{{ m + 1 }}</td>
-          <td v-for="(column, i) in columns" :key="i" class="f-table__td">
-            {{ dataItem[column.key] }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <main :class="['f-table__body', { 'f-table__body-margin': height }]">
+        <table>
+          <thead v-if="!height" :align="align">
+            <tr>
+              <th v-if="num">序号</th>
+              <th v-for="(column, index) in columns" :key="index">
+                {{ column.title }}
+              </th>
+            </tr>
+          </thead>
+
+          <tbody :align="align">
+            <tr v-for="(dataItem, m) in data" :key="m">
+              <td v-if="num">{{ m + 1 }}</td>
+              <td v-for="(column, i) in columns" :key="i">
+                {{ dataItem[column.key] }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+    </div>
   </div>
 </template>
