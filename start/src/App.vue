33f5a78ac431a1fff96d6118c907251dd3913d87
tyh2001 <1469442737@qq.com>
2022-11-10 16:13:52 +0800

    refactor: 优化样式细节 🔒🔒🔒

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,22 +1,16 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
 
-  const type = ref('primary')
-
-  const change = (): void => {
-    type.value = 'default'
-  }
+  const value = ref('选项1')
 </script>
 
 <template>
-  <f-button :type="type" :click="change">主要按钮</f-button>
-
-  <!-- <f-select v-model="value">
+  <f-select v-model="value">
     <f-option value="选项1"></f-option>
     <f-option disabled value="选项2"></f-option>
     <f-option value="选项3"></f-option>
     <f-option value="选项4"></f-option>
-  </f-select> -->
+  </f-select>
 </template>
 
 <style lang="scss" scoped></style>
