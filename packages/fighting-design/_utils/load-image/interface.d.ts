7f4658761c215638f0757fc3786a8830638bff97
tyh2001 <1469442737@qq.com>
2022-11-10 10:33:54 +0800

    style: 优化 ts 类型和注释规范 ⚠️⚠️⚠️

--- packages/fighting-design/_utils/load-image/interface.d.ts
+++ packages/fighting-design/_utils/load-image/interface.d.ts
@@ -1,3 +1,7 @@
+import type { HandleEventInterface, OrdinaryFunctionInterface } from '../../_interface'
+
+export type { HandleEventInterface, OrdinaryFunctionInterface } from '../../_interface'
+
 /**
  * Load 加载类所需要的 props 参数
  * 
@@ -13,7 +17,7 @@
  * 
  * error 图片加载失败触发的回调
  */
-export interface LoadNeedImagePropsInterface {
+export interface LoadImagePropsInterface {
   src: string
   errSrc: string
   rootMargin: string
@@ -30,14 +34,6 @@ export interface LoadCallbackInterface {
 }
 
 /**
- * 懒加载类接口
- */
-export interface LoadLazyInterface {
-  observer(): IntersectionObserver
-  lazyCreateImg(): void
-}
-
-/**
  * 加载图片方法接口
  * 
  * node 所需要设置的 img 节点
@@ -49,9 +45,44 @@ export interface LoadLazyInterface {
 export interface LoadImageInterface {
   (
     node: HTMLImageElement,
-    prop: LoadNeedImagePropsInterface,
+    prop: LoadImagePropsInterface,
     callback: CallbackInterface | null
   ): void
+}
+
+/**
+ * 开始加载图片类型接口
+ * 
+ * 用于 Load 的 loadCreateImg 类方法
+ * 
+ * 可接收一个可选的 errSrc 参数
+ * 
+ * 如果有 errSrc 则加载备用地址，否则加载 src
+ */
+export interface LoadCreateImgInterface {
+  (errSrc?: string): void
+}
+
+/**
+ * 加载图片方法类型接口
+ * 
+ * evt 事件对象
+ * 
+ * src 图片地址
+ */
+export interface LoadOnloadInterface {
+  (evt: Event, src: string): void
+}
+
+/**
+ * 懒加载函数类型接口
+ * 
+ * 无参数，返回 IntersectionObserver
+ * 
+ * https://developer.mozilla.org/zh-CN/docs/Web/API/IntersectionObserver/IntersectionObserver
+ */
+export interface LoadLazyObserverInterface {
+  (): IntersectionObserver
 }
 
 /**
@@ -59,9 +90,17 @@ export interface LoadImageInterface {
  */
 export interface LoadInterface {
   node: HTMLImageElement
-  props: LoadNeedImagePropsInterface
+  props: LoadImagePropsInterface
   callback: CallbackInterface | null
-  loadCreateImg(errSrc?: string): void
-  onerror(evt: Event): void
-  onload(evt: Event, src: string): void
+  loadCreateImg: LoadCreateImgInterface
+  onerror: HandleEventInterface
+  onload: LoadOnloadInterface
+}
+
+/**
+ * 懒加载类接口
+ */
+export interface LoadLazyInterface {
+  observer: LoadLazyObserverInterface
+  lazyCreateImg: OrdinaryFunctionInterface
 }
