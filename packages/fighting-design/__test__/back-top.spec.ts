91a24618cd70b0f23b83a975835f55115abef3b7
tyh2001 <1469442737@qq.com>
2022-11-12 10:43:21 +0800

    test: 更新单元测试 🦘🦘🦘

--- packages/fighting-design/__test__/back-top.spec.ts
+++ packages/fighting-design/__test__/back-top.spec.ts
@@ -5,42 +5,42 @@ import { FBackTop } from '../index'
 describe('FBackTop', () => {
   test('class', () => {
     const wrapper = mount(FBackTop)
-    expect(wrapper.classes()).toContain('f-back-top')
+    expect(wrapper.find('div').classes()).toContain('f-back-top')
   })
 
   test('round', () => {
     const wrapper = mount(FBackTop, {
       props: { round: true }
     })
-    expect(wrapper.classes()).toContain('f-back-top__round')
+    expect(wrapper.find('div').classes()).toContain('f-back-top__round')
   })
 
   test('right', () => {
     const wrapper = mount(FBackTop, {
       props: { right: '100px' }
     })
-    expect(wrapper.attributes('style')).toContain('100px')
+    expect(wrapper.find('div').attributes('style')).toContain('100px')
   })
 
   test('bottom', () => {
     const wrapper = mount(FBackTop, {
       props: { bottom: '100px' }
     })
-    expect(wrapper.attributes('style')).toContain('100px')
+    expect(wrapper.find('div').attributes('style')).toContain('100px')
   })
 
   test('z-index', () => {
     const wrapper = mount(FBackTop, {
       props: { zIndex: 300 }
     })
-    expect(wrapper.attributes('style')).toContain(300)
+    expect(wrapper.find('div').attributes('style')).toContain(300)
   })
 
   test('background', () => {
     const wrapper = mount(FBackTop, {
       props: { background: 'red' }
     })
-    expect(wrapper.attributes('style')).toContain(
+    expect(wrapper.find('div').attributes('style')).toContain(
       'red'
     )
   })
@@ -49,7 +49,7 @@ describe('FBackTop', () => {
     const wrapper = mount(FBackTop, {
       props: { color: 'orange' }
     })
-    expect(wrapper.attributes('style')).toContain(
+    expect(wrapper.find('div').attributes('style')).toContain(
       'orange'
     )
   })
