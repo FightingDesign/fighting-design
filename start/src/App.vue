c1b4c5c64736dd8669ba60cf31b0dcf57e9ce9dc
tyh2001 <1469442737@qq.com>
2022-11-13 09:26:27 +0800

    style: 优化 message 样式规范 ⚖️⚖️⚖️

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,16 +1,36 @@
-<script lang="ts" setup></script>
+<script setup lang="ts">
+  import { FMessage } from '../../packages/fighting-design/index'
+
+  const openMessagePlacement = (placement) => {
+    FMessage({
+      message: '这是一条自定义位置的消息',
+      type: 'primary',
+      close: true,
+      duration: 0,
+      placement: placement
+    })
+  }
+</script>
 
 <template>
-  <f-dropdown>
-    <f-button type="primary">下拉菜单</f-button>
-
-    <template #content>
-      <f-dropdown-item>猕猴桃</f-dropdown-item>
-      <f-dropdown-item>哈密瓜</f-dropdown-item>
-      <f-dropdown-item>火龙果</f-dropdown-item>
-      <f-dropdown-item>柠檬</f-dropdown-item>
-    </template>
-  </f-dropdown>
+  <f-space>
+    <f-button type="primary" @click="openMessagePlacement('top')">
+      顶部
+    </f-button>
+    <f-button type="primary" @click="openMessagePlacement('top-left')">
+      左上
+    </f-button>
+    <f-button type="primary" @click="openMessagePlacement('top-right')">
+      右上
+    </f-button>
+    <f-button type="success" @click="openMessagePlacement('bottom')">
+      底部
+    </f-button>
+    <f-button type="success" @click="openMessagePlacement('bottom-left')">
+      左下
+    </f-button>
+    <f-button type="success" @click="openMessagePlacement('bottom-right')">
+      右下
+    </f-button>
+  </f-space>
 </template>
-
-<style lang="scss" scoped></style>
