d5d26f7d1b553d0a6aeeb26dde387e10f1ecb4c5
lukecheng <lukecheng233@163.com>
2022-11-16 03:46:43 +0800

    feat: slider组件测试版

--- packages/fighting-design/slider/src/slider.vue
+++ packages/fighting-design/slider/src/slider.vue
@@ -49,20 +49,29 @@
   const styleList: ComputedRef<CSSProperties> = computed((): CSSProperties => {
     const { min, max, disabled} = prop
 
-    const selectedRightValue: string = ((percent.value - min) * 100 / (max - min)) + '%'
+    const leftIconPercent = percent.value[0] * 100 + '%'
+    const rightIconPercent = percent.value[1] * 100 + '%'
 
     const cursorType = disabled ? 'not-allowed' : isDraging.value ? 'grabbing' : 'grab'
 
     const styles = {
-      '--f-slider-selected-right-value': selectedRightValue,
+      '--f-slider-left-icon-percent': leftIconPercent,
+      '--f-slider-right-icon-percent': rightIconPercent,
       '--f-slider-cursor-type': cursorType
     } as CSSProperties
 
     return styles
   })
-  
+
   // 动画百分比
-  const percent = ref(prop.modelValue)
+  const percent = ref([0, 0])
+  onMounted(() => {
+    const { min, max, modelValue} = prop
+    const leftIconPercent = ((modelValue[0] - min) / (max - min))
+    const rightIconPercent = ((modelValue[1] - min) / (max - min))
+
+    percent.value = [leftIconPercent, rightIconPercent]
+  })
   // 是否开启滑动缓冲动画
   const isOpenAnimation = ref<'width .3s' | ''>('width .3s')
   // 是否正在拖动
@@ -72,46 +81,47 @@
     const fSliderLeft = FSlider.value.offsetLeft
     const fSliderWidth = FSlider.value.clientWidth
 
-    const {min, max} = prop
-
     let distanceX = e.pageX - fSliderLeft
+    const rightIconLeft = fSliderWidth * percent.value[1]
+
     if(distanceX < 0) distanceX = 0
-    if(distanceX > fSliderWidth) distanceX = fSliderWidth
+    if(distanceX > (rightIconLeft)) distanceX = rightIconLeft
 
     isOpenAnimation.value = ''
-    percent.value = min + (max - min) * (distanceX / fSliderWidth)
+    percent.value[0] = (distanceX / fSliderWidth)
+
   }
 
   const cancelDrag = (e): void => {
     if(!isDraging.value || prop.disabled) return
-
-    // 卸载 拖动监听
+    /**
+     0. 左控件的offsetLeft，是相对它的定位父元素的。
+     1. 相对原先滑块，移动的距离是多少？
+     2. 一个步长的宽度是多少？
+     3. 相对原先滑块，移动的步长是多少？
+     4. 移动后的滑块占百分比多少？
+     */
     document.removeEventListener('mousemove', drag, true)
     document.body.style.cursor = ''
-    
     const fSliderLeft = FSlider.value.offsetLeft
     const fSliderWidth = FSlider.value.clientWidth
-
-    const {min, max, step} = prop
-
+    const rightIconLeft = fSliderWidth * percent.value[1]
     let distanceX = e.pageX - fSliderLeft
     if(distanceX < 0) distanceX = 0
-    if(distanceX > fSliderWidth) distanceX = fSliderWidth
-
-    const stepWidth = fSliderWidth / ((max - min) / step)
-
-    const stepNum = distanceX / stepWidth
-
-    const decimal = (stepNum % 1) > 0.5 ? 1 : 0
+    if(distanceX > (rightIconLeft)) distanceX = rightIconLeft
+    const {min, max, step} = prop
+    const stepWidth = fSliderWidth * (step / (max - min))
+    const rightIconStepNum = rightIconLeft / stepWidth
+    let stepNum = distanceX / stepWidth
+    let decimal = stepNum % 1
+    stepNum = parseInt(stepNum + '') + (decimal > 0.5 ? 1 : 0)
+    if(stepNum >= rightIconStepNum) stepNum -= 1
+    const newPercent = stepNum / ((max - min))
+    console.log(newPercent);
     
-    const newModelValue = min + (parseInt(stepNum + '') + decimal) * step
+    percent.value[0] = newPercent
+    // console.log(stepNum, '移动步长');
     
-    isOpenAnimation.value = 'width .3s'
-    percent.value = newModelValue
-
-    isDraging.value = false
-
-    emit('update:modelValue', newModelValue)
   }
 
   // 取消拖动监听
@@ -119,12 +129,11 @@
     // 卸载 取消拖动监听
     onBeforeUnmount(() => document.removeEventListener('mouseup', cancelDrag, true))
 
-  const onRightIconMousedown = (e): void => {
+  const onLeftIconMousedown = (): void => {
     if(prop.disabled) return
 
     isDraging.value = true
     document.body.style.cursor = 'grabbing'
-    drag(e)
     // 拖动监听
     document.addEventListener('mousemove', drag, true)
   }
@@ -134,13 +143,14 @@
 </script>
 
 <template>
-  <div ref="FSlider" :class="classList" :style="styleList" @mousedown="onRightIconMousedown">
-    <div class="f-slider__selected" :style="`transition: ${isOpenAnimation}`">
-      <div class="selected-left-icon"></div>
-      <div 
-        class="selected-right-icon"
-        @mousedown="onRightIconMousedown"
-      ></div>
-    </div>
+  <div ref="FSlider" :class="classList" :style="styleList">
+    <div 
+      class="f-slider__left__icon"
+      @mousedown="onLeftIconMousedown"
+    ></div>
+    <div class="f-slider__selected"></div>
+    <div 
+      class="f-slider__right__icon"
+    ></div>
   </div>
 </template>
--- start/src/App.vue
+++ start/src/App.vue
@@ -2,18 +2,15 @@
   import { FSlider } from '@/index'
 import { ref } from 'vue';
 
-  const value = ref(25)
+  const value = ref([0, 20])
 
   const disabled = ref(false)
 </script>
 
 <template>
   <div style="padding: 0 100px">
-    <f-slider v-model="value" :disabled="disabled" :vertical="false" :step="1" :min="0" :max="100"></f-slider>
+    <f-slider v-model="value" :step="1" :min="0" :max="100"></f-slider>
 
     <button @click="disabled=!disabled">禁用</button>
-  </div>
-  <div style="height: 500px;">
-    <f-slider vertical></f-slider>
   </div>
 </template>
