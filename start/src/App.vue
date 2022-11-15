524e4ccc17ddaa135839ae4458323a2ed1ae9110
gz_lib <3437705679@qq.com>
2022-11-15 22:00:02 +0800

    feat: 轮播图

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,33 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="home-banner">
-    <f-carousel :data="sliders">
-      <template #body="{ row }">
-        <img :src="row.url" alt="" />
-      </template>
-    </f-carousel>
-  </div>
-</template>
-<script>
-  import { ref } from 'vue'
-  export default {
-    name: 'HomeBanner',
-    setup() {
-      const sliders = ref([
-        {
-          url: 'https://picsum.photos/200/300'
-        },
-        {
-          url: 'https://picsum.photos/200/300'
-        },
-        {
-          url: 'https://picsum.photos/200/300'
-        }
-      ])
-      return { sliders }
-    }
-  }
-=======
   <f-checkbox-group v-model="option4" border size="large">
     <f-checkbox label="选项一" />
     <f-checkbox label="选项二" />
@@ -51,8 +22,8 @@
   import { ref } from 'vue'
 
   const option4 = ref([])
->>>>>>> 92e60848925d51c8c70da68f94b2048c6dc69649
 </script>
+
 <style lang="css">
   body {
     margin: 0;
