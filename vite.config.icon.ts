154e77d164a8285bd228e34f36cfb4b329f93f44
tyh2001 <1469442737@qq.com>
2022-11-17 16:31:59 +0800

    chore: 更新 svg 打包配置项 🛸🛸🛸

--- vite.config.icon.ts
+++ vite.config.icon.ts
@@ -1,7 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader' // https://github.com/jpkleemans/vite-svg-loader
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+// import dts from 'vite-plugin-dts'
 import { copyFileSync } from 'fs'
 import { version } from './packages/fighting-icon/package.json'
 import type { UserConfigExport } from 'vite'
@@ -10,16 +10,16 @@ export default (): UserConfigExport => {
   return {
     plugins: [
       vue(),
-      dts({
-        insertTypesEntry: true, // 是否生成类型声明入口
-        cleanVueFileName: true, // 是否将 '.vue.d.ts' 文件名转换为 '.d.ts'
-        copyDtsFiles: true, // 是否将源码里的 .d.ts 文件复制到 outputDir
-        include: ['./packages/fighting-icon'], // 手动设置包含路径的 glob
-        // 构建后回调钩子
-        afterBuild: (): void => {
-          move()
-        }
-      }),
+      // dts({
+      //   insertTypesEntry: true, // 是否生成类型声明入口
+      //   cleanVueFileName: true, // 是否将 '.vue.d.ts' 文件名转换为 '.d.ts'
+      //   copyDtsFiles: true, // 是否将源码里的 .d.ts 文件复制到 outputDir
+      //   include: ['./packages/fighting-icon'], // 手动设置包含路径的 glob
+      //   // 构建后回调钩子
+      //   afterBuild: (): void => {
+      //     move()
+      //   }
+      // }),
       svgLoader()
     ],
     build: {
@@ -57,3 +57,5 @@ const move = (): void => {
 
   console.warn('\n' + `Fighting Icon ${version} 版本打包成功 🎉🎉🎉` + '\n')
 }
+
+setTimeout(() => move(), 3000)
