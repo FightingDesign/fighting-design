9d84ee6f12bee637cdf967c324873fc0ce0c89b6
tyh2001 <1469442737@qq.com>
2022-11-18 16:45:31 +0800

    perf: 优化 start 类型配置文件 🌟🌟🌟

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,17 +1,5 @@
 <script lang="ts" setup></script>
 
-<template>
-  <f-button type="default">默认按钮</f-button>
-  <f-button type="primary">主要按钮</f-button>
-  <f-button type="success">成功按钮</f-button>
-  <f-button type="danger">危险按钮</f-button>
-  <f-button type="warning">警告按钮</f-button>
-
-  <f-alert type="default">默认提示信息</f-alert>
-  <f-alert type="primary">主要提示信息</f-alert>
-  <f-alert type="success">成功提示信息</f-alert>
-  <f-alert type="danger">危险提示信息</f-alert>
-  <f-alert type="warning">警告提示信息</f-alert>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
