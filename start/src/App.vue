3afae4a651193dc5e9aaf43eae22f3a6737f6618
tyh2001 <1469442737@qq.com>
2022-11-19 22:40:52 +0800

    perf: 优化 popup 组件样式细节 🍡🍡🍡

--- start/src/App.vue
+++ start/src/App.vue
@@ -6,6 +6,13 @@
   const click = (a): void => {
     console.log(a)
   }
+
+  const visible2 = ref(false)
+  const direction = ref('center')
+
+  const onShow = () => {
+    visible2.value = true
+  }
 </script>
 
 <template>
@@ -17,8 +24,14 @@
 
   <input v-model="value5" type="date" /> -->
 
-  <!-- <f-date-picker v-model:date="value5"></f-date-picker> -->
+  <f-date-picker v-model:date="value5"></f-date-picker>
   <f-time-picker v-model:time="value5"></f-time-picker>
+
+  <f-button type="primary" @click="onShow">从下往上开</f-button>
+
+  <f-popup v-model:visible="visible2" :direction="direction" :padding="30">
+    <h3>h12</h3>
+  </f-popup>
 </template>
 
 <style>
