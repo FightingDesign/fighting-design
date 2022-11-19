a872d179b6556e4e39290512e4c145e3a0c35924
tyh2001 <1469442737@qq.com>
2022-11-19 23:26:16 +0800

    feat: 完善 time-picker 组件 🚔🚔🚔

--- packages/fighting-design/time-picker/src/time-picker.vue
+++ packages/fighting-design/time-picker/src/time-picker.vue
@@ -1,34 +1,71 @@
 <script lang="ts" setup name="FTimePicker">
   import { Props } from './props'
-  import { ref, watch } from 'vue'
+  import { ref, watch, reactive } from 'vue'
   import { FInput } from '../../input'
   import { FDropdown } from '../../dropdown'
-  import type { TimePickerPropsType } from './interface'
+  import { addZero, isString } from '../../_utils'
   import type { Ref } from 'vue'
+  import type {
+    TimePickerPropsType,
+    TimePickerTimeListInterface,
+    TimePickerHandleClickInterface
+  } from './interface'
 
   const prop: TimePickerPropsType = defineProps(Props)
   const emit = defineEmits({
-    'update:time': (val: string): string => val
+    'update:time': (val: string): boolean => isString(val)
   })
 
+  // 获取当前的时间
+  const nowDate: Date = new Date()
   // 选择的日期
   const pickerTime: Ref<string> = ref<string>(prop.time)
+  // 当前小时
+  const timeList: TimePickerTimeListInterface =
+    reactive<TimePickerTimeListInterface>({
+      hover: addZero(nowDate.getHours()),
+      minute: addZero(nowDate.getMinutes())
+    })
 
   /**
    * 监视绑定值，发生变化之后同步数据
    */
   watch(
     (): string => pickerTime.value,
-    (newVal): void => {
+    (newVal: string): void => {
       emit('update:time', newVal)
     }
   )
+
+  /**
+   * 监视 timeList 对象发生变化同步最新选取的时间
+   */
+  watch(
+    (): TimePickerTimeListInterface => timeList,
+    (): void => {
+      pickerTime.value = `${timeList.hover}:${timeList.minute}`
+    },
+    { deep: true }
+  )
+
+  /**
+   * 点击对应的时间进行选取
+   *
+   * @param date 当前的时间数字
+   * @param params 小时还是分钟
+   */
+  const handleClick: TimePickerHandleClickInterface = (
+    date: number,
+    params: 'hover' | 'minute'
+  ): void => {
+    timeList[params] = addZero(date)
+  }
 </script>
 
 <template>
   <div class="f-time-picker">
     <f-dropdown>
-      <f-input v-model="pickerTime" type="text" readonly />
+      <f-input v-model="pickerTime" type="text" readonly clear />
 
       <template #content>
         <div class="f-time-picker__content">
@@ -38,8 +75,9 @@
               v-for="hour in 24"
               :key="hour"
               class="f-time-picker__hour-item"
+              @click.stop="handleClick(hour, 'hover')"
             >
-              {{ hour - 1 }}
+              {{ addZero(hour - 1) }}
             </div>
           </div>
 
@@ -49,8 +87,9 @@
               v-for="minute in 60"
               :key="minute"
               class="f-time-picker__minute-item"
+              @click.stop="handleClick(minute, 'minute')"
             >
-              {{ minute }}
+              {{ addZero(minute) }}
             </div>
           </div>
         </div>
--- start/src/App.vue
+++ start/src/App.vue
@@ -3,35 +3,25 @@
 
   const value5 = ref('')
 
-  const click = (a): void => {
-    console.log(a)
-  }
-
-  const visible2 = ref(false)
-  const direction = ref('center')
-
-  const onShow = () => {
-    visible2.value = true
-  }
 </script>
 
 <template>
-  <f-button type="primary" :click="click">主要按钮</f-button>
-
   <!-- <h1>{{ value5 }}</h1> -->
 
   <!-- <f-calendar v-model:date="date" />
 
   <input v-model="value5" type="date" /> -->
 
-  <f-date-picker v-model:date="value5"></f-date-picker>
+  <input v-model="value5" type="time" />
+
+  <!-- <f-date-picker v-model:date="value5"></f-date-picker> -->
   <f-time-picker v-model:time="value5"></f-time-picker>
 
-  <f-button type="primary" @click="onShow">从下往上开</f-button>
+  <!-- <f-button type="primary" @click="onShow">从下往上开</f-button>
 
   <f-popup v-model:visible="visible2" :direction="direction" :padding="30">
     <h3>h12</h3>
-  </f-popup>
+  </f-popup> -->
 </template>
 
 <style>
