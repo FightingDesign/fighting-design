1a3f50eb06303607414b6da516729763af4af203
tyh2001 <1469442737@qq.com>
2022-11-26 14:41:40 +0800

    feat: 新增类型导出 📍📍📍

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,17 +1,5 @@
-<script lang="ts" setup>
-  import { ref } from 'vue'
+<script lang="ts" setup></script>
 
-  const value = ref([0, 20])
+<template></template>
 
-  const disabled = ref(false)
-</script>
-
-<template>
-  <div style="padding: 0 100px">
-    <f-slider v-model="value" :step="1" :min="0" :max="20"></f-slider>
-    <br />
-    <br />
-    <br />
-    <button @click="disabled = !disabled">禁用</button>
-  </div>
-</template>
+<style lang="scss" scoped></style>
