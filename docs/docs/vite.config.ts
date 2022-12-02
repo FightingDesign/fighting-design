98b112d47ac2ec5cd9863acef28865676b567ba6
tyh2001 <1469442737@qq.com>
2022-12-02 13:42:43 +0800

    chore: 更新文档配置项 ☔☔☔

--- docs/docs/vite.config.ts
+++ docs/docs/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import vueSetupExtend from 'unplugin-vue-setup-extend-plus/vite'
 import type { UserConfigExport } from 'vite'
 
@@ -10,7 +11,15 @@ export default (): UserConfigExport => {
       exclude: ['vitepress']
     },
     server: {
-      port: 9999 // 端口号
+      port: 9999, // 端口号
+      fs: {
+        /**
+         * 可以为项目根目录的上一级提供服务
+         * 
+         * @see allow https://cn.vitejs.dev/config/server-options.html#server-fs-allow
+         */
+        allow: [resolve(__dirname, '..', '..')]
+      }
     },
     css: {
       postcss: {
