ca3f54a92361c29c1c567ca25c3e52de388c1f3a
tyh2001 <1469442737@qq.com>
2022-12-14 19:14:22 +0800

    refactor: 更新插件注释

--- docs/docs/.vitepress/config/index.ts
+++ docs/docs/.vitepress/config/index.ts
@@ -4,7 +4,12 @@ import { pluginRender } from './plugin-render'
 import { pluginTable } from './plugin-table'
 import type MarkdownIt from 'markdown-it'
 
-export const fightingPlugin = (md: MarkdownIt) => {
+/**
+ * 导出自定义插件方法
+ * 
+ * @param md markdown 实例
+ */
+export const fightingPlugin = (md: MarkdownIt): void => {
   md.use(pluginDemo)
   md.use(pluginCode)
   md.use(pluginRender)
