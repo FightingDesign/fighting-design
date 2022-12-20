21052b90b51b1a7d8091d02e7dee46d777127213
tyh2001 <1469442737@qq.com>
2022-12-20 08:50:14 +0800

    docs: 更新 form 组件文档

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,25 +1,41 @@
 <script lang="ts" setup>
   import { reactive } from 'vue'
-  import type { FormParam } from 'fighting-design'
+  import { FMessage } from 'fighting-design'
 
   const ruleForm = reactive({
     account: '',
     password: ''
   })
 
-  const handelSubmit = ({ ok, res, evt }: FormParam): void => {
+  const handelSubmit = ({ ok, res, evt }): void => {
     if (!ok) return
-    console.log(ok, res, evt, '开始提交表单')
+    FMessage.success(`ok: ${ok} res: ${res} evt: ${evt} 开始提交表单`)
   }
 </script>
 
 <template>
-  <f-form label-width="60px" :model="ruleForm" :on-submit="handelSubmit">
-    <f-form-item label="账号">
+  {{ ruleForm }}
+  <f-form :model="ruleForm" label-width="60px" :on-submit="handelSubmit">
+    <f-form-item
+      label="账号"
+      name="account"
+      :rules="[
+        { required: true, message: '请输入用户名' },
+        { min: 4, max: 12, message: '请输入 4~12 用户名' },
+        { regExp: /123456/, message: '必须包含 123456' }
+      ]"
+    >
       <f-input v-model="ruleForm.account" type="text" placeholder="请输入账号" />
     </f-form-item>
 
-    <f-form-item label="密码">
+    <f-form-item
+      label="密码"
+      name="password"
+      :rules="[
+        { required: true, message: '请输入密码' },
+        { min: 6, message: '至少输入六位数密码' }
+      ]"
+    >
       <f-input v-model="ruleForm.password" type="password" placeholder="请输入密码" />
     </f-form-item>
 
