96fa8ee3cde5babd27bedb1884c8384533c928cf
tyh2001 <1469442737@qq.com>
2022-12-29 15:53:33 +0800

    refactor: 清除 start 🌞🌞🌞

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,24 +1,5 @@
-<script lang="ts" setup>
-  import { ref } from 'vue'
-  import type { Ref } from 'vue'
-  import type { FightingType } from 'fighting-design'
+<script lang="ts" setup></script>
 
-  const typeList: Ref<FightingType[]> = ref<FightingType[]>(['default', 'primary', 'success', 'danger', 'warning'])
-</script>
+<template></template>
 
-<template>
-  <f-ripple v-for="(item, i) in typeList" :key="i" :type="item">
-    <div class="list">
-      <f-text :type="item">{{ item }} 涟漪</f-text>
-    </div>
-  </f-ripple>
-</template>
-
-<style scoped>
-  .list {
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
+<style lang="scss" scoped></style>
