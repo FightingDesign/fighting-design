82ae952acfdf4e97db663dbf36fede8c0fe03137
lukecheng <lukecheng233@163.com>
2023-01-03 14:12:01 +0800

    feat: 完善单元测试

--- packages/fighting-design/slider/__test__/slider.spec.ts
+++ packages/fighting-design/slider/__test__/slider.spec.ts
@@ -4,7 +4,29 @@ import { FSlider } from '../index'
 
 describe('FSlider', () => {
   test('class', () => {
-    const wrapper = mount(FSlider)
-    expect(wrapper.classes()).toContain('f-slider')
+    const wrapper = mount(FSlider, {
+      props: { modelValue: 0 }
+    })
+    expect(wrapper.find('div').classes()).toContain('f-slider')
+  })
+
+  test('disabled', () => {
+    const wrapper = mount(FSlider, {
+      props: { 
+        modelValue: 0,
+        disabled: true
+      }
+    })
+    expect(wrapper.find('.f-slider').classes()).toContain('f-slider__disabled')
+  })
+
+  test('bgColor', () => {
+    const wrapper = mount(FSlider, {
+      props: { 
+        modelValue: 0,
+        bgColor: '#bfa'
+      }
+    })
+    expect(wrapper.find('.f-slider').attributes('style')).toContain('--f-slider-bg-color: #bfa')
   })
 })
