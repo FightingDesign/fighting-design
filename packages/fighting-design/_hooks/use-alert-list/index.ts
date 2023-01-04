062a4716322e6dc1cf1750289e020d5377682704
tyh2001 <1469442737@qq.com>
2023-01-04 18:36:31 +0800

    style: 优化 hooks 注释规范 🍧🍧🍧

--- packages/fighting-design/_hooks/use-alert-list/index.ts
+++ packages/fighting-design/_hooks/use-alert-list/index.ts
@@ -6,7 +6,6 @@ export * from './interface.d'
  * 处理 alert 组件滚动列表方法
  *
  * @param node 滚动节点
- * @returns
  */
 export const useAlertList = (node: HTMLElement): UseAlertListReturn => {
   /**
@@ -19,41 +18,29 @@ export const useAlertList = (node: HTMLElement): UseAlertListReturn => {
    * @see appendChild https://developer.mozilla.org/zh-CN/docs/Web/API/Node/appendChild
    */
   const firstClone = (): void => {
-    /**
-     * 获取到第一个元素
-     */
+    /** 获取到第一个元素 */
     const firstChild: HTMLElement = node.children[0] as HTMLElement
-    /**
-     * 克隆一个新的元素
-     */
+    /** 克隆一个新的元素 */
     const newChild: HTMLElement = firstChild.cloneNode(true) as HTMLElement
     node.appendChild(newChild)
   }
 
   firstClone()
 
-  /**
-   * 滚动距离
-   */
+  /** 滚动距离 */
   const top = 24
-  /**
-   * 滚动到第几个
-   */
+  /** 滚动到第几个 */
   let scrollIndex = 0
-  /**
-   * 子节点的数量
-   */
+  /** 子节点的数量 */
   const nodeChildrenLength = node.children.length
 
-  /**
-   * 滚动元素
-   */
+  /** 滚动元素 */
   const moveNext = (): void => {
     scrollIndex++
 
-    // 判断如果到了最后一个元素
+    /** 判断如果到了最后一个元素 */
     if (scrollIndex === nodeChildrenLength - 1) {
-      // 等到滚动动画结束再返回，避免动画丢失
+      /** 等到滚动动画结束再返回，避免动画丢失 */
       setTimeout((): void => {
         scrollIndex = 0
         node.scrollTo({ top: 0, behavior: 'auto' })
@@ -66,7 +53,6 @@ export const useAlertList = (node: HTMLElement): UseAlertListReturn => {
 
   /**
    * 开始触发滚动
-   *
    * @param duration 滚动间隔时间
    */
   const startMove = (duration = 2000): void => {
--- packages/fighting-design/_hooks/use-alert-list/interface.d.ts
+++ packages/fighting-design/_hooks/use-alert-list/interface.d.ts
@@ -1,5 +1,7 @@
 /**
  * useAlertList 返回值类型接口
+ * 
+ * @param startMove 开始触发滚动
  */
 export interface UseAlertListReturn {
   startMove: (duration?: number) => void
