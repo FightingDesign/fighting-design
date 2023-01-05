96082f8d45ae568044618334dd138adf042823c8
tyh2001 <1469442737@qq.com>
2023-01-05 21:06:11 +0800

    chore: 更新 postcss 配置 🚏🚏🚏

--- vite.config.css.ts
+++ vite.config.css.ts
@@ -1,11 +1,23 @@
 import { resolve } from 'path'
 import { readdirSync } from 'fs'
+import postcssPresetEnv from 'postcss-preset-env' // https://www.npmjs.com/package/postcss-preset-env
+import autoprefixer from 'autoprefixer' // https://www.npmjs.com/package/autoprefixer
 import type { UserConfigExport } from 'vite'
 
 const input: string = resolve(__dirname, 'packages/fighting-theme/src')
 
 export default (): UserConfigExport => {
   return {
+    css: {
+      postcss: {
+        plugins: [
+          /** 针对一些前沿的 css 进行降级 */
+          postcssPresetEnv(),
+          /** 自动添加浏览器厂商前缀 */
+          autoprefixer()
+        ]
+      }
+    },
     build: {
       assetsDir: 'theme',
       rollupOptions: {
