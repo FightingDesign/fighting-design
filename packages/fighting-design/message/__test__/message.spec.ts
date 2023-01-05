a3a952be13cf4e1da84e906e268798d08672ae43
lukecheng <lukecheng233@163.com>
2023-01-05 15:12:51 +0800

    test: 更新单元测试 🍁🍁🍁

--- packages/fighting-design/message/__test__/message.spec.ts
+++ packages/fighting-design/message/__test__/message.spec.ts
@@ -1,11 +1,13 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
-import FMessageVue, { FMessage } from '../../index'
-import type { Ref } from 'vue'
+import { FMessage } from '../../index'
+import FMessageVue from '../src/message.vue'
 
-// type MessageInstance = ComponentPublicInstance<{
-//   visible: boolean
-// }>
+import type { ComponentPublicInstance, Ref } from 'vue'
+
+type MessageInstance = ComponentPublicInstance<{
+  visible: boolean
+}>
 
 const sleep = (time: number): Promise<unknown> => {
   return new Promise(resolve => {
@@ -21,12 +23,12 @@ describe('FMessage', () => {
         type: 'success'
       }
     })
-    expect(wrapper.find('.f-message').exists()).toBe(false)
-    expect(wrapper.find('.f-message__success').exists()).toBe(false)
+    expect(wrapper.find('.f-message').exists()).toBe(true)
+    expect(wrapper.find('.f-message__success').exists()).toBe(true)
     await wrapper.setProps({ type: 'primary' })
-    expect(wrapper.find('.f-message__primary').exists()).toBe(false)
+    expect(wrapper.find('.f-message__primary').exists()).toBe(true)
     await wrapper.setProps({ close: true })
-    expect(wrapper.find('.f-message__close').exists()).toBe(false)
+    expect(wrapper.find('.f-message__close').exists()).toBe(true)
   })
 
   test('click-close', async () => {
@@ -39,19 +41,23 @@ describe('FMessage', () => {
       }
     })
 
-    expect(wrapper.find('.f-message__close').exists()).toBe(false)
-    // expect(wrapper.find('.f-message').isVisible()).toBe(true)
-    // await wrapper.find('.f-message__-close').trigger('click')
-    // expect((wrapper.vm as unknown as MessageInstance).visible).toBe(false)
+    expect(wrapper.find('.f-message__close').exists()).toBe(true)
+    expect(wrapper.find('.f-message').isVisible()).toBe(true)
+    await wrapper.find('.f-message__close').trigger('click')
+    expect((wrapper.vm as unknown as MessageInstance).visible).toBe(false)
   })
 
   test('use func', async () => {
     const messageReturn = FMessage({
-      message: 'message-test'
+      message: 'message-test',
+      type: 'success'
     })
 
-    expect(messageReturn).toHaveProperty('close')
+    expect(messageReturn).toHaveProperty('id')
     expect(messageReturn).toHaveProperty('vm')
+    expect(messageReturn).toHaveProperty('close')
+    expect(messageReturn.close).toBeTypeOf('function')
+    expect(messageReturn).toHaveProperty('bottom')
     expect(messageReturn.vm.exposed).toHaveProperty('visible')
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
