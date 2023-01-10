79de4ae496f96890e72e46ab4998b2b649a633f8
tyh2001 <1469442737@qq.com>
2023-01-10 18:03:32 +0800

    refactor: 优化配置文件注释 🌂🌂🌂

--- vite.config.css.ts
+++ vite.config.css.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { readdirSync } from 'fs'
-import postcssPresetEnv from 'postcss-preset-env' // https://www.npmjs.com/package/postcss-preset-env
-import autoprefixer from 'autoprefixer' // https://www.npmjs.com/package/autoprefixer
+import postcssPresetEnv from 'postcss-preset-env' // 
+import autoprefixer from 'autoprefixer' // 
 import type { UserConfigExport } from 'vite'
 
 const input: string = resolve(__dirname, 'packages/fighting-theme/src')
@@ -10,13 +10,40 @@ export default (): UserConfigExport => {
   return {
     /** 针对 css 的配置项 */
     css: {
+      /**
+       * 配置 postcss
+       * 
+       * @see css.postcss https://cn.vitejs.dev/config/shared-options.html#css-postcss
+       */
       postcss: {
         plugins: [
-          /** 针对一些前沿的 css 进行降级 */
+          /** 
+           * 针对一些前沿的 css 进行降级
+           * 
+           * @see postcss-preset-env https://www.npmjs.com/package/postcss-preset-env
+           */
           postcssPresetEnv(),
-          /** 自动添加浏览器厂商前缀 */
+          /** 
+           * 自动添加浏览器厂商前缀
+           * 
+           * @see autoprefixer https://www.npmjs.com/package/autoprefixer
+           */
           autoprefixer()
         ]
+      },
+      /**
+       * @see css.preprocessorOptions https://cn.vitejs.dev/config/shared-options.html#css-preprocessoroptions
+       */
+      preprocessorOptions: {
+        scss: {
+          additionalData: '$injectedColor: orange;'
+        }
+      },
+      /**
+       * @see css.modules https://cn.vitejs.dev/config/shared-options.html#css-modules
+       */
+      modules: {
+        scopeBehaviour: 'local'
       }
     },
     build: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,89 @@
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts' // https://github.com/qmhc/vite-plugin-dts
+import dts from 'vite-plugin-dts'
 import FightingSetupNamePlugin from './packages/fighting-setup-name'
 import { resolve } from 'path'
 import { copyFileSync } from 'fs'
 import { name, version } from './packages/fighting-design/package.json'
-import { visualizer } from 'rollup-plugin-visualizer' // https://github.com/btd/rollup-plugin-visualizer
+import { visualizer } from 'rollup-plugin-visualizer'
 import type { UserConfigExport } from 'vite'
 
 export default (): UserConfigExport => {
   return {
+    /**
+     * 插件配置
+     * 
+     * @see plugins https://cn.vitejs.dev/config/shared-options.html#plugins
+     */
     plugins: [
+      /**
+       * vue 插件
+       * 
+       * @see vite-plugin-vue https://github.com/vitejs/vite-plugin-vue
+       */
       vue(),
+      /**
+       * 打包类型
+       * 
+       * @see vite-plugin-dts https://github.com/qmhc/vite-plugin-dts
+       */
       dts({
-        skipDiagnostics: true, // 是否跳过类型诊断
-        staticImport: true, // 是否将动态引入转换为静态
-        outputDir: ['./dist/lib', './dist/es'], // 可以指定一个数组来输出到多个目录中
-        insertTypesEntry: true, // 是否生成类型声明入口
-        cleanVueFileName: true, // 是否将 '.vue.d.ts' 文件名转换为 '.d.ts'
-        copyDtsFiles: true, // 是否将源码里的 .d.ts 文件复制到 outputDir
-        include: ['./packages/fighting-design'], // 手动设置包含路径的 glob
-        // 构建后回调钩子
+        skipDiagnostics: true, /** 是否跳过类型诊断 */
+        staticImport: true, /** 是否将动态引入转换为静态 */
+        outputDir: ['./dist/lib', './dist/es'], /** 可以指定一个数组来输出到多个目录中 */
+        insertTypesEntry: true, /** 是否生成类型声明入口 */
+        cleanVueFileName: true, /** 是否将 '.vue.d.ts' 文件名转换为 '.d.ts' */
+        copyDtsFiles: true, /** 是否将源码里的 .d.ts 文件复制到 outputDir */
+        include: ['./packages/fighting-design'], /** 手动设置包含路径的 glob */
+        /** 构建后回调钩子 */
         afterBuild: (): void => {
           move()
         }
       }),
+      /** 可设置组件名称 */
       FightingSetupNamePlugin(),
+      /**
+       * 可视化捆绑包
+       * 
+       * @see rollup-plugin-visualizer https://github.com/btd/rollup-plugin-visualizer
+       */
       visualizer()
     ],
+    /**
+     * 构建配置项
+     * 
+     * @see 构建选项 https://cn.vitejs.dev/config/build-options.html
+     */
     build: {
-      target: 'modules', // 这是指 支持原生 ES 模块、原生 ESM 动态导入
-      minify: true, // 压缩代码
-      chunkSizeWarningLimit: 2, // 打包的组件超过 2kb 警告提示
-      reportCompressedSize: true, // 启用 gzip 压缩大小报告
+      target: 'modules', /** 这是指 支持原生 ES 模块、原生 ESM 动态导入 */
+      minify: true, /** 压缩代码 */
+      chunkSizeWarningLimit: 2, /** 打包的组件超过 2kb 警告提示 */
+      reportCompressedSize: true, /** 启用 gzip 压缩大小报告 */
       emptyOutDir: false,
-      outDir: resolve(__dirname, './dist'), // 指定输出路径
-      // 库模式 https://cn.vitejs.dev/guide/build.html#library-mode
+      outDir: resolve(__dirname, './dist'), /** 指定输出路径 */
+      /**
+       * 库模式
+       * 
+       * @see 库模式 https://cn.vitejs.dev/guide/build.html#library-mode
+       */
       lib: {
         name: 'FightingDesign',
-        entry: resolve(__dirname, 'packages/fighting-design/index.ts') // 打包入口
+        entry: resolve(__dirname, 'packages/fighting-design/index.ts') /** 打包入口 */
       },
-      // rollup 配置项 https://rollupjs.org/guide/en/#big-list-of-options
+      /**
+       * rollup 配置项 
+       * 
+       * @see big-list-of-options https://rollupjs.org/guide/en/#big-list-of-options
+       */
       rollupOptions: {
-        external: ['vue'], // 确保外部化处理那些你不想打包进库的依赖 https://rollupjs.org/guide/en/#external
+        /**
+         * 确保外部化处理那些你不想打包进库的依赖
+         * 
+         * @see external https://rollupjs.org/guide/en/#external
+         */
+        external: ['vue'],
         output: [
           {
-            name: 'FightingDesign', // 包名
+            name: 'FightingDesign', /** 包名 */
             format: 'umd',
             exports: 'named',
             sourcemap: false,
@@ -54,21 +93,66 @@ export default (): UserConfigExport => {
             assetFileNames: '[name].[ext]',
             manualChunks: undefined,
             inlineDynamicImports: false,
-            globals: { vue: 'Vue' }, // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+            globals: { vue: 'Vue' }, /** 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量 */
             namespaceToStringTag: true
           },
           {
-            format: 'es', // 打包模式 https://rollupjs.org/guide/en/#outputformat
-            exports: 'named', // 导出模式 https://rollupjs.org/guide/en/#outputexports
-            dir: 'dist/es', // 输出路径 https://rollupjs.org/guide/en/#outputdir
-            sourcemap: false, // https://rollupjs.org/guide/en/#outputsourcemap
-            entryFileNames: 'index.js', // 输出后的文件名 https://rollupjs.org/guide/en/#outputentryfilenames
-            chunkFileNames: '[name].js', // 输出的 chunk文件名 https://rollupjs.org/guide/en/#outputchunkfilenames
-            assetFileNames: '[name].[ext]', // 输出资产文件名 https://rollupjs.org/guide/en/#outputassetfilenames
-            inlineDynamicImports: false, // https://rollupjs.org/guide/en/#outputinlinedynamicimports
+            /**
+             * 打包模式
+             * 
+             * @see output.format https://rollupjs.org/guide/en/#outputformat
+             */
+            format: 'es',
+            /**
+             * 导出模式
+             * 
+             * @see output.exports https://rollupjs.org/guide/en/#outputexports
+             */
+            exports: 'named',
+            /**
+             * 输出路径
+             * 
+             * @see output.outputdir https://rollupjs.org/guide/en/#outputdir
+             */
+            dir: 'dist/es',
+            /** 
+             * @see output.outputsourcemap https://rollupjs.org/guide/en/#outputsourcemap
+             */
+            sourcemap: false,
+            /**
+             * 输出后的文件名
+             * 
+             * @see output.outputentryfilenames https://rollupjs.org/guide/en/#outputentryfilenames
+             */
+            entryFileNames: 'index.js',
+            /**
+             * 输出的 chunk文件名
+             * 
+             * @see output.outputchunkfilenames https://rollupjs.org/guide/en/#outputchunkfilenames
+             */
+            chunkFileNames: '[name].js',
+            /**
+             * 输出资产文件名
+             * 
+             * @see output.outputassetfilenames https://rollupjs.org/guide/en/#outputassetfilenames
+             */
+            assetFileNames: '[name].[ext]',
+            /**
+             * @see output.outputinlinedynamicimports https://rollupjs.org/guide/en/#outputinlinedynamicimports
+             */
+            inlineDynamicImports: false,
+            /**
+             * @see output.outputmanualchunks https://rollupjs.org/guide/en/#outputmanualchunks
+             */
             manualChunks: undefined,
-            preserveModules: true, // https://rollupjs.org/guide/en/#outputpreservemodules
-            namespaceToStringTag: true // https://rollupjs.org/guide/en/#outputnamespacetostringtag
+            /**
+             * @see output.outputpreservemodules https://rollupjs.org/guide/en/#outputpreservemodules
+             */
+            preserveModules: true,
+            /**
+             * @see output.outputnamespacetostringtag https://rollupjs.org/guide/en/#outputnamespacetostringtag
+             */
+            namespaceToStringTag: true
           },
           {
             format: 'cjs',
@@ -89,13 +173,11 @@ export default (): UserConfigExport => {
   }
 }
 
+/** 打包结束之后将一些静态文件进行移入 */
 const move = (): void => {
   const files = [
     { input: './README.md', outDir: 'dist/README.md' },
-    {
-      input: './packages/fighting-design/package.json',
-      outDir: 'dist/package.json'
-    },
+    { input: './packages/fighting-design/package.json', outDir: 'dist/package.json' },
     { input: './LICENSE', outDir: 'dist/LICENSE' }
   ] as const
 
