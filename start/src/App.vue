bd95fc166523784b56281c86e8752be634a9df09
tyh2001 <1469442737@qq.com>
2023-01-11 11:47:42 +0800

    refactor: 更新 lint 配置文件 🥟🥟🥟

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,14 +1,5 @@
-<script lang="ts" setup>
-  import { ref } from 'vue'
+<script lang="ts" setup></script>
 
-  const value1 = ref(3)
-</script>
+<template></template>
 
-<template>
-  <f-input-number model="switch" size="large" />
-  <f-input-number model="switch" size="middle" />
-  <f-input-number model="switch" size="small" />
-  <f-input-number model="switch" size="mini" />
-  <!-- <f-input-number v-model="value1" model="switch" /> -->
-  <!-- <f-input-number v-model="value1" model="button" /> -->
-</template>
+<style lang="scss" scoped></style>
