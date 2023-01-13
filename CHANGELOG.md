8dc41445af7af4599c6af93fbe1feb45e69b4317
tyh2001 <1469442737@qq.com>
2023-01-13 10:47:19 +0800

    perf: 优化 dropdown-item 样式细节 💉💉💉

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 中文 | [英文](https://github.com/FightingDesign/fighting-design/blob/master/CHANGELOG.en-US.md)
 
+- 优化 `f-dropdown-item` 样式细节
+- 优化 `f-trigger` 实现
+
 ## 0.19.0 (2023-01-12)
 
 **优化**
--- start/src/App.vue
+++ start/src/App.vue
@@ -1,21 +1,5 @@
 <script lang="ts" setup></script>
 
-<template>
-  <!-- <f-trigger trigger="hover">
-    <f-button type="primary">hover 触发</f-button>
-
-    <template #content>
-      <f-empty content="在这里，后续提供更多服务" />
-    </template>
-  </f-trigger> -->
-
-  <f-trigger trigger="hover" style="margin-left: 120px">
-    <f-button type="primary">click 触发</f-button>
-
-    <template #content>
-      <f-empty content="在这里，后续提供更多服务" />
-    </template>
-  </f-trigger>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
