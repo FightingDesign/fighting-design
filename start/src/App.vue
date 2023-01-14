74424d66013c7cbdd3a9cc33da22f46934556901
tyh2001 <1469442737@qq.com>
2023-01-14 10:27:02 +0800

    refactor: 清楚 start 😶😶😶

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,13 +1,5 @@
 <script lang="ts" setup></script>
 
-<template>
-  <f-trigger trigger="click" style="margin-left: 120px">
-    <f-button type="primary">click 触发</f-button>
-
-    <template #content>
-      <f-empty content="在这里，后续提供更多服务" />
-    </template>
-  </f-trigger>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
