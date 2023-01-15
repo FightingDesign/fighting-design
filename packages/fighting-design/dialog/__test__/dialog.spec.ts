1095e2bc3f552aa2c127b57adad9cc88485ee96b
lukecheng <lukecheng233@163.com>
2023-01-15 15:49:26 +0800

    feat: 完善单元测试 🥥🥥🥥

--- packages/fighting-design/dialog/__test__/dialog.spec.ts
+++ packages/fighting-design/dialog/__test__/dialog.spec.ts
@@ -1,25 +1,44 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
 import { FDialog } from '../index'
+  import { EMIT_VISIBLE } from '../../_tokens'
 
 describe('FDialog', () => {
   test('class', () => {
-    const wrapper = mount(FDialog)
+    const wrapper = mount(FDialog, {
+      props: {
+        visible: true
+      }
+    })
     expect(wrapper.find('div').classes()).toContain('f-popup')
   })
 
   test('title', () => {
     const wrapper = mount(FDialog, {
       props: {
+        visible: true,
         title: '标题'
       }
     })
     expect(wrapper.find('.f-dialog__header-title').text()).toContain('标题')
   })
 
+  test('appendToBody', () => {
+    const wrapper = mount(FDialog, {
+      props: {
+        visible: true,
+        title: '标题',
+        appendToBody: true
+      }
+    })
+    expect(wrapper.find('.f-popup .f-dialog').exists()).toBe(false)
+    expect(document.body.innerHTML).toContain('f-dialog')
+  })
+
   test('width', () => {
     const wrapper = mount(FDialog, {
       props: {
+        visible: true,
         width: '200px'
       }
     })
@@ -29,6 +48,7 @@ describe('FDialog', () => {
   test('width', () => {
     const wrapper = mount(FDialog, {
       props: {
+        visible: true,
         width: 400
       }
     })
@@ -38,9 +58,35 @@ describe('FDialog', () => {
   test('fullscreen', () => {
     const wrapper = mount(FDialog, {
       props: {
+        visible: true,
         fullscreen: true
       }
     })
     expect(wrapper.find('.f-dialog').classes()).toContain('f-dialog__fullscreen')
+  })
+
+  test('showMask', () => {
+    const wrapper = mount(FDialog, {
+      props: {
+        visible: true,
+        showMask: true
+      }
+    })
+    expect(wrapper.find('.f-popup__mask').exists()).toBeTruthy()
+  })
+
+  test('maskClose', async () => {
+    const wrapper = mount(FDialog, {
+      props: {
+        visible: true,
+        showMask: true
+      }
+    })
+    await wrapper.get('.f-popup__container').trigger('click.self')
+    expect(wrapper.emitted(EMIT_VISIBLE)?.[0][0]).toBe(false)
+    await wrapper.setProps({ visible: false })
+    // 应该使用 js-dom。如果使用 happy-dom，下面的测试就会失败。
+    // 查看这个 issue https://github.com/vuejs/test-utils/issues/1704
+    expect(wrapper.find('.f-dialog').isVisible()).toBe(false)
   })
 })
