ea803345c470be0d88304ecd54bf89ac265e5525
lukecheng <lukecheng233@163.com>
2023-01-15 17:51:51 +0800

    test: 完善单元测试 🧨🧨🧨

--- packages/fighting-design/message/__test__/message.spec.ts
+++ packages/fighting-design/message/__test__/message.spec.ts
@@ -42,7 +42,9 @@ describe('FMessage', () => {
     })
 
     expect(wrapper.find('.f-message__close').exists()).toBe(true)
-    expect(wrapper.find('.f-message').exists()).toBe(true)
+    // 等待过渡动画结束
+    await sleep(400)
+    expect(wrapper.find('.f-message').isVisible()).toBe(true)
     await wrapper.find('.f-message__close').trigger('click')
     expect((wrapper.vm as unknown as MessageInstance).visible).toBe(false)
   })
