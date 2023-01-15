20ed06a7f54778cf30448741456631ffaeda1540
tyh2001 <1469442737@qq.com>
2023-01-15 15:32:55 +0800

    refactor: 优化 date-picker 回调抽离 💡💡💡

--- packages/fighting-design/_tokens/emits/index.ts
+++ packages/fighting-design/_tokens/emits/index.ts
@@ -11,3 +11,10 @@ export const EMIT_UPDATE = 'update:modelValue' as const
  * 主要用于控制显示隐藏
  */
 export const EMIT_VISIBLE = 'update:visible' as const
+
+/**
+ * emit 回调方法名称
+ * 
+ * 用于更新日期
+ */
+export const EMIT_DATE = 'update:date' as const
