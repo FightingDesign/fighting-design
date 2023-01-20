54e9bc4a967561374f9dd2611d472d2af5e6fc75
tyh2001 <1469442737@qq.com>
2023-01-20 09:46:29 +0800

    test: 新增单元测试 ⭐⭐⭐

--- packages/fighting-design/tooltip/__test__/tooltip.spec.ts
+++ packages/fighting-design/tooltip/__test__/tooltip.spec.ts
@@ -5,10 +5,9 @@ import { FTooltip } from '../index'
 describe('FTooltip', () => {
   test('class', () => {
     const wrapper = mount(FTooltip, {
-      slots: {
-        default: () => 'test'
-      }
+      slots: { default: 'test' }
     })
     expect(wrapper.classes()).toContain('f-tooltip')
+    expect(wrapper.attributes('role')).toContain('tooltip')
   })
 })
