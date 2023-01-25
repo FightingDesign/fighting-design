a8cc46902fb5d6c3a7a9abaf6b2d41d4d56d6494
田同学 <1469442737@qq.com>
2023-01-25 09:51:14 +0800

    style：清除 start 🐥🐥🐥

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,14 +1,3 @@
-<script lang="ts" setup>
-  import { FMessage } from 'fighting-design'
+<script lang="ts" setup></script>
 
-  const openMessage = (): void => {
-    FMessage({
-      message: '这是一条消息提示',
-      type: 'success'
-    })
-  }
-</script>
-
-<template>
-  <f-button type="primary" :on-click="openMessage">show</f-button>
-</template>
+<template></template>
