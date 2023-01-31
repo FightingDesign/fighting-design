e2ff9bbe17b1f84a3136a28ecdf99c69995daa92
tyh2001 <1469442737@qq.com>
2023-01-31 16:20:32 +0800

    style: 优化 alert 组件样式 🌨️🌨️🌨️

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,5 +1,17 @@
 <script lang="ts" setup></script>
 
-<template></template>
+<template>
+  <f-alert round type="default">默认提示信息</f-alert>
+  <f-alert type="primary">主要提示信息</f-alert>
+  <f-alert type="success">成功提示信息</f-alert>
+  <f-alert type="danger">危险提示信息</f-alert>
+  <f-alert type="warning">警告提示信息</f-alert>
+
+  <f-alert :font-size="12" type="default">默认提示信息</f-alert>
+  <f-alert :font-size="14" type="primary">主要提示信息</f-alert>
+  <f-alert :font-size="16" type="success">成功提示信息</f-alert>
+  <f-alert font-size="20px" type="danger">危险提示信息</f-alert>
+  <f-alert font-size="24px" type="warning">警告提示信息</f-alert>
+</template>
 
 <style lang="scss" scoped></style>
