34e9a40b70229e1d485aa6aae3bf430a9f82c30d
tyh2001 <1469442737@qq.com>
2023-02-01 14:22:27 +0800

    style: 优化样式表 🔥🔥🔥

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,5 +1,29 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue'
 
-<template></template>
+  const num = ref(10)
 
-<style lang="scss" scoped></style>
+  const sun = () => {
+    if (num.value > 0) {
+      num.value -= 10
+    }
+  }
+
+  const add = () => {
+    if (num.value < 100) {
+      num.value += 10
+    }
+  }
+</script>
+
+<template>
+  <f-progress :height="25" stripe type="primary" :percentage="num" />
+  <f-progress :height="25" stripe type="success" :percentage="num" />
+  <f-progress :height="25" stripe type="danger" :percentage="num" />
+  <f-progress :height="25" stripe type="warning" :percentage="num" />
+
+  <f-button-group>
+    <f-button simple round type="primary" :on-click="sun">减少</f-button>
+    <f-button simple round type="primary" :on-click="add">增加</f-button>
+  </f-button-group>
+</template>
