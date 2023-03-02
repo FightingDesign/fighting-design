f1b564e5f1c04b73379a306cca4b3a204e03bd47
tyh2001 <1469442737@qq.com>
2023-03-02 18:36:26 +0800

    fix: 修复类型 🤪🤪🤪

--- packages/fighting-design/input-number/src/props.ts
+++ packages/fighting-design/input-number/src/props.ts
@@ -1,5 +1,6 @@
 import { setBooleanProp, setStringProp, setNumberProp, setFunctionProp } from '../../_utils'
 import { FIGHTING_SIZE, FIGHTING_TYPE } from '../../_tokens'
+import type { InputEvent } from '../../input'
 import type { ExtractPropTypes } from 'vue'
 import type { InputNumberModel } from './interface'
 import type { HandleEvent, FightingSize, InputChange, FightingType } from '../../_interface'
@@ -84,7 +85,7 @@ export const Props = {
   /** 获得焦点时触发的回调 */
   onFocus: setFunctionProp<HandleEvent>(),
   /** input 事件触发的回调 */
-  onInput: setFunctionProp<HandleEvent>()
+  onInput: setFunctionProp<InputEvent>()
 } as const
 
 /** input-number 组件 props 类型 */
