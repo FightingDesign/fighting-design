997b966ec91bf450f21b7295008d33383f79e93f
Tyh2001 <1469442737@qq.com>
2023-05-01 18:55:26 +0800

    fix: 修复 `f-avatar` 组件 `round` 对于 `text` 失效问题

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,5 +1,38 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { reactive } from 'vue'
+  import { FMessage } from 'fighting-design'
 
-<template></template>
+  const ruleForm = reactive({
+    account: '',
+    password: ''
+  })
 
-<style lang="scss" scoped></style>
+  const handelSubmit = (): void => {
+    FMessage.primary('开始提交表单')
+  }
+</script>
+
+<template>
+  <f-avatar src="https://tianyuhao.cn/images/auto/my.jpg" />
+  <f-avatar round text="你好" />
+
+  {{ ruleForm }}
+  <f-form label-width="0px" label-position="top" :on-submit="handelSubmit">
+    <f-form-item label="请输入账号">
+      <f-input
+        v-model="ruleForm.account"
+        :max-length="6"
+        type="text"
+        placeholder="请输入账号"
+      />
+    </f-form-item>
+
+    <f-form-item>
+      <f-input v-model="ruleForm.password" type="password" placeholder="请输入密码" />
+    </f-form-item>
+
+    <f-form-item>
+      <f-button type="primary" native-type="submit">提交表单</f-button>
+    </f-form-item>
+  </f-form>
+</template>
