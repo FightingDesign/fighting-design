b1672755926a277bf2d3559f1eb5bb5601b1a5cd
Tyh2001 <1469442737@qq.com>
2023-05-10 13:51:06 +0800

    chore: 更新文件名和样式

--- packages/fighting-add-component/template/component/index.ts
+++ packages/fighting-add-component/template/component/index.ts
@@ -6,6 +6,6 @@ export const F<%= displayName %> = install(<%= displayName %>)
 
 export type <%= displayName %>Instance = InstanceType<typeof <%= displayName %>>
 
-export * from './src/interface.d'
+export * from './src/interface'
 
 export default F<%= displayName %>
--- packages/fighting-design/count-down/index.ts
+++ packages/fighting-design/count-down/index.ts
@@ -7,6 +7,6 @@ export const FCountDown = install(CountDown)
 /** count-down 组件实例类型 */
 export type CountDownInstance = InstanceType<typeof CountDown>
 
-export * from './src/interface.d'
+export * from './src/interface'
 
 export default FCountDown
