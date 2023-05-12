723ba5eefb3a4e44d2cf18387a57346a9f00a0d5
JetTsang <1109126828@qq.com>
2023-05-12 20:12:16 +0800

    feat: count-down 组件增加渲染间隔 interval 可选参数

--- packages/fighting-design/count-down/src/props.ts
+++ packages/fighting-design/count-down/src/props.ts
@@ -10,6 +10,8 @@ import type { CountDownFinish } from './interface'
 export const Props = {
   /** 倒计时时长，单位毫秒 */
   time: setStringNumberProp(0),
+  /** 倒计时渲染间隔，单位毫秒 */
+  interval: setStringNumberProp(1000),
   /** 时间格式 */
   format: setStringProp<string>('HH:mm:ss'),
   /** 是否自动开始倒计时 */
