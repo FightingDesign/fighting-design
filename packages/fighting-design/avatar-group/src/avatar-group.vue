e7fc77d5b9dcb45027a52ee57e02b6150d6a9172
tyh666999 <1469442737@qq.com>
2023-07-08 07:13:47 +0800

    style: 优化注释细节

--- packages/fighting-design/avatar-group/src/avatar-group.vue
+++ packages/fighting-design/avatar-group/src/avatar-group.vue
@@ -11,21 +11,31 @@
 
   provide(AVATAR_GROUP_PROPS_KEY, prop)
 
-  /** 样式列表 */
-  const styleList = styles(['positionSize'])
-
+  /** 头像组元素节点 */
   const avatarGroupRef = ref<HTMLDivElement | undefined>()
 
-  onMounted(() => {
+  /** 设置元素的层级样式 */
+  const setZIndexStyle = (): void => {
     if (avatarGroupRef.value) {
+      /** 获取到所有的子元素 */
       const avatars = avatarGroupRef.value.querySelectorAll(
         '.f-avatar'
       ) as unknown as HTMLDivElement[]
-      avatars.forEach((item: HTMLDivElement, i: number): void => {
-        item.style.zIndex = (avatars.length - i).toString()
-      })
+
+      /** 必须在子元素存在的时候，设置 zIndex 层级样式 */
+      if (avatars && avatars.length) {
+        avatars.forEach((item: HTMLDivElement, i: number): void => {
+          item.style.zIndex = (avatars.length - i).toString()
+        })
+      }
     }
-  })
+  }
+
+  /** dom 挂载之后设置样式 */
+  onMounted(setZIndexStyle)
+
+  /** 样式列表 */
+  const styleList = styles(['positionSize'])
 </script>
 
 <template>
