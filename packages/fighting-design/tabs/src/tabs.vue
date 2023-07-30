57746880185998b5db51efaf245538be849f5c86
Tyh2001 <1469442737@qq.com>
2023-07-30 22:39:33 +0800

    ci: 更新 ci

--- packages/fighting-design/tabs/src/tabs.vue
+++ packages/fighting-design/tabs/src/tabs.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
   import { Props, TABS_PROPS_KEY } from './props'
-  import { provide, getCurrentInstance, ref, isVNode, computed } from 'vue'
+  import { provide, getCurrentInstance, ref, isVNode, computed, shallowRef } from 'vue'
   import { isObject, isArray, isBoolean } from '../../_utils'
-  import { useList } from '../../_hooks'
+  import { useList, useRun } from '../../_hooks'
+  import { FCloseBtn } from '../../close-btn'
+  import { FIconPlus } from '../../_svg'
+  import { FSvgIcon } from '../../svg-icon'
   import type { TabsItemProps } from '../../tabs-item'
   import type {
     ComponentInternalInstance,
@@ -10,7 +13,12 @@
     Component,
     VNodeNormalizedChildren
   } from 'vue'
-  import type { TabsOpts, TabsProvide, TabsChildrenItem } from './interface'
+  import type {
+    TabsOpts,
+    TabsProvide,
+    // TabsChildrenItem,
+    TabsModelValue
+  } from './interface'
 
   defineOptions({ name: 'FTabs' })
 
@@ -21,12 +29,13 @@
   })
 
   const { classes, styles } = useList(prop, 'tabs')
+  const { run } = useRun()
 
   /** 当前选中的 name */
   const activeName = ref<string | number>(0)
 
   const childrenMap = new Map<number, TabsOpts>()
-  const children = ref<TabsChildrenItem[]>()
+  const children = shallowRef<TabsOpts[]>()
 
   const flattedChildren = (children: VNode | VNodeNormalizedChildren): VNode[] => {
     const vNodes = isArray(children) ? children : [children]
@@ -59,10 +68,9 @@
 
   const root = getCurrentInstance() as ComponentInternalInstance
   const component = 'FTabsItem'
-
+  
   const registerChild = (opts: TabsOpts): void => {
     childrenMap.set(opts.uid, opts)
-
     const componentList: VNode[] = getChildrenComponent(root, component)
 
     const componentUid: number[] = componentList
@@ -71,20 +79,9 @@
       })
       .filter(Boolean) as number[]
 
-    children.value = (
-      componentUid
-        .map((e: number): TabsOpts | undefined => {
-          return childrenMap.get(e)
-        })
-        .filter(Boolean) as TabsOpts[]
-    ).map((item: TabsOpts, index: number): TabsChildrenItem => {
-      item.activeName = item.activeName || index
-
-      return {
-        name: item.activeName || index,
-        label: item.label
-      } as const
-    })
+    children.value = componentUid
+      .map((e: number): TabsOpts | undefined => childrenMap.get(e))
+      .filter(Boolean) as TabsOpts[]
 
     if (modelValue.value) {
       activeName.value = modelValue.value
@@ -92,11 +89,27 @@
     }
 
     if (children.value && children.value.length) {
-      if (children.value[0].name) {
-        activeName.value = children.value[0].name
+      if (children.value[0].activeName) {
+        activeName.value = children.value[0].activeName
       }
     }
   }
+
+  /** nav 列表 */
+  const navs = computed(() => {
+    return (
+      children.value &&
+      children.value.map((item: TabsOpts, index: number) => {
+        item.activeName = item.activeName || index
+
+        return {
+          /** name 如果没有传递 则用索引代替 */
+          name: item.activeName,
+          label: item.label
+        } as const
+      })
+    )
+  })
 
   /** 将信息传递给子组件 */
   provide<TabsProvide>(TABS_PROPS_KEY, {
@@ -132,6 +145,21 @@
   const trigger = computed((): 'click' | 'mouseenter' => {
     return prop.trigger === 'hover' ? 'mouseenter' : 'click'
   })
+
+  /**
+   * 添加或删除卡片
+   *
+   * @param { 'remove' | 'add' } action 添加或删除
+   * @param { string | number } [name] 标签 name
+   * @param { number } [index] 索引
+   */
+  const editItem = (
+    action: 'remove' | 'add',
+    name?: TabsModelValue,
+    index?: number
+  ): void => {
+    run(prop.onEdit, action, name, index)
+  }
 </script>
 
 <template>
@@ -146,7 +174,7 @@
       <!-- 标签列表 -->
       <div class="f-tabs__navs">
         <div
-          v-for="(item, index) in children"
+          v-for="(item, index) in navs"
           :key="index"
           :class="[
             'f-tabs__nav-item',
@@ -155,6 +183,22 @@
           @[trigger]="changeNavs(item.name)"
         >
           {{ item.label }}
+
+          <!-- 关闭按钮 -->
+          <f-close-btn
+            v-if="type === 'card' && editStatus"
+            round
+            @click.stop="editItem('remove', item.name, index)"
+          />
+        </div>
+
+        <!-- 卡片样式编辑状态下的添加按钮 -->
+        <div
+          v-if="type === 'card' && editStatus"
+          class="f-tabs__nav-item"
+          @click="editItem('add')"
+        >
+          <f-svg-icon :icon="FIconPlus" color="#666" />
         </div>
       </div>
 
--- start/src/App.vue
+++ start/src/App.vue
@@ -1,3 +1,41 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue'
 
-<template></template>
+  let tabIndex = 2
+
+  const list = ref([
+    { label: '第一个', content: '内容 1', name: '1' },
+    { label: '第二个', content: '内容 2', name: '2' }
+  ])
+
+  const onEdit = (action: 'remove' | 'add', name: string, index: number): void => {
+    switch (action) {
+      case 'add':
+        {
+          const newTabName = `${++tabIndex}`
+          list.value.push({
+            label: '新的' + newTabName,
+            content: '新的标签页' + newTabName,
+            name: newTabName
+          })
+        }
+        break
+      case 'remove':
+        list.value.splice(index, 1)
+        break
+    }
+  }
+</script>
+
+<template>
+  <f-tabs edit-status type="card" :on-edit="onEdit">
+    <f-tabs-item
+      v-for="(item, index) in list"
+      :key="index"
+      :label="item.label"
+      :name="item.name"
+    >
+      {{ item.content }}
+    </f-tabs-item>
+  </f-tabs>
+</template>
