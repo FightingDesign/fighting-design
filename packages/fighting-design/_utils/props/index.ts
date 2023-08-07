8e562527f66c3e949a2b8f21045bd7b7b3cdc390
tyh666999 <1469442737@qq.com>
2023-08-07 13:13:49 +0800

    style: 优化 props 设置方法

--- packages/fighting-design/_utils/props/index.ts
+++ packages/fighting-design/_utils/props/index.ts
@@ -4,6 +4,14 @@ import type { PropType } from 'vue'
  * 设置组件的 prop 参数
  *
  * @see 为组件的props标注类型 https://cn.vuejs.org/guide/typescript/options-api.html#typing-component-props
+ * 
+ * 注意：你需要了解 null 和 undefined 的区别
+ * 
+ * null       表示无对象  表示 no object
+ * undefined  表示空值    表示 no value
+ * 
+ * 如果一个值将来可能会是一个对象，但是目前还不是，则使用 null，也就是说 Object 类型的默认值都需要使用 null
+ * 基本类型都使用 undefined 默认值
  */
 
 /**
@@ -27,16 +35,13 @@ export const setBooleanProp = (
  * @param { number } [defaultVal = undefined] 默认值
  * @returns { Object } 配置对象
  */
-export const setNumberProp = <T extends number>(
+export const setNumberProp = (
   defaultVal?: number
 ): {
-  type: NumberConstructor
-  default: T extends number ? number : null
+  readonly type: NumberConstructor
+  readonly default: number | undefined
 } => {
-  return { type: Number, default: defaultVal } as {
-    readonly type: NumberConstructor
-    readonly default: T extends number ? number : null
-  }
+  return { type: Number, default: defaultVal } as const
 }
 
 /**
@@ -89,28 +94,28 @@ export const setStringNumberProp = <T extends string | number>(
 }
 
 /**
- * 设置 object 类型 props 参数
- *
- * @param { Object } [defaultVal = undefined] 默认值
- * @returns { Object } 配置对象
- */
-export const setObjectProp = <T extends object>(
-  defaultVal?: T
-): {
-  readonly type: PropType<T>
-  readonly default: () => T | undefined
-} => {
-  return {
-    type: Object as PropType<T>,
-    default: (): T | undefined => defaultVal
-  } as const
-}
-
-/**
  * 为了保证组件实例之间的独立性和数据隔离，避免因为共享引用导致的副作用和意外修改
  *
  * 引用类似默认值使用函数返回
  */
+
+/**
+ * 设置 object 类型 props 参数
+ *
+ * @param { Object } [defaultVal = null] 默认值
+ * @returns { Object } 配置对象
+ */
+export const setObjectProp = <T extends object>(
+  defaultVal = null
+): {
+  readonly type: PropType<T>
+  readonly default: () => T | null
+} => {
+  return {
+    type: Object as PropType<T>,
+    default: (): T | null => defaultVal
+  } as const
+}
 
 /**
  * 设置 function 类型 props 参数
@@ -119,31 +124,31 @@ export const setObjectProp = <T extends object>(
  * @returns { Object } 配置对象
  */
 export const setFunctionProp = <T extends Function>(
-  defaultVal?: T
+  defaultVal = null
 ): {
   readonly type: PropType<T>
-  readonly default: () => T | undefined
+  readonly default: () => T | null
 } => {
   return {
     type: Function as PropType<T>,
-    default: (): T | undefined => defaultVal
+    default: (): T | null => defaultVal
   } as const
 }
 
 /**
  * 设置 array 类型 props 参数
  *
- * @param { Array } [defaultVal = undefined] 默认值
+ * @param { Array } [defaultVal = null] 默认值
  * @returns { Object } 配置对象
  */
 export const setArrayProp = <T>(
-  defaultVal?: undefined
+  defaultVal = null
 ): {
   readonly type: PropType<T>
-  readonly default: () => T | undefined
+  readonly default: () => T | null
 } => {
   return {
     type: Array as unknown as PropType<T>,
-    default: (): T | undefined => defaultVal
+    default: (): T | null => defaultVal
   } as const
 }
