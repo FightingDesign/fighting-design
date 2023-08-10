f01e0237f1d89e262ef30bf3420633c3e3a039c9
tyh666999 <1469442737@qq.com>
2023-08-10 17:54:58 +0800

    style: 优化 radio 和 checkbox 样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,3 +1,37 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue'
 
-<template></template>
+  const radio4 = ref('1')
+</script>
+
+<template>
+  <f-radio-group v-model="radio4" background size="large">
+    <f-radio label="1">备选项1</f-radio>
+    <f-radio label="2">备选项2</f-radio>
+    <f-radio label="3">备选项3</f-radio>
+  </f-radio-group>
+  <br />
+  <br />
+
+  <f-radio-group v-model="radio4" background size="middle">
+    <f-radio label="1">备选项1</f-radio>
+    <f-radio label="2">备选项2</f-radio>
+    <f-radio label="3">备选项3</f-radio>
+  </f-radio-group>
+  <br />
+  <br />
+
+  <f-radio-group v-model="radio4" background size="small">
+    <f-radio label="1">备选项1</f-radio>
+    <f-radio label="2">备选项2</f-radio>
+    <f-radio label="3">备选项3</f-radio>
+  </f-radio-group>
+  <br />
+  <br />
+
+  <f-radio-group v-model="radio4" background size="mini">
+    <f-radio label="1">备选项1</f-radio>
+    <f-radio label="2">备选项2</f-radio>
+    <f-radio label="3">备选项3</f-radio>
+  </f-radio-group>
+</template>
