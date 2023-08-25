ee80d3db6f77d5091b58f783f97c075d75403b8e
Tyh2001 <1469442737@qq.com>
2023-08-25 22:30:27 +0800

    feat: 优化 `f-dialog` 组件样式

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,12 +1,3 @@
-<script lang="ts" setup>
-  import { ref } from 'vue'
+<script lang="ts" setup></script>
 
-  const value = ref(30)
-</script>
-
-<template>
-  {{ value }}
-  <f-select v-model="value" placeholder="请选择……">
-    <f-option v-for="i in 5" :key="i" :value="i">选项 {{ i }}</f-option>
-  </f-select>
-</template>
+<template></template>
