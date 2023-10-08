2760076aa127e9d63594d2d6af96b00caf04689a
tyh666999 <1469442737@qq.com>
2023-10-08 11:22:10 +0800

    style: 更新

--- start/src/main.ts
+++ start/src/main.ts
@@ -4,11 +4,7 @@ import App from './App.vue'
 // 开发阶段引入
 // import fighting from '../../packages/fighting-design/index'
 
-// core()
-
-import FightingDesign from '../../dist/packages/fighting-design/index.js'
-
-// import fighting from 'fighting-design'
+import FightingDesign from '../../dist/src/index.js'
 
 console.log(FightingDesign)
 
