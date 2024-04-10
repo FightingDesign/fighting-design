8a45a294bb6ba055ceec6792fc1c84a197b62900
w2xi <43wangxi@gmail.com>
2024-04-10 13:47:16 +0800

    test(components): [layout] cover remaining direction cases

--- packages/fighting-design/layout/__test__/layout.spec.ts
+++ packages/fighting-design/layout/__test__/layout.spec.ts
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
 import { FLayout } from '../index'
+import FHeader from '../../header'
+import FFooter from '../../footer'
 
 describe('f-layout', () => {
   test('class', () => {
@@ -8,15 +10,36 @@ describe('f-layout', () => {
     expect(wrapper.classes()).toContain('f-layout')
   })
 
-  test('direction', () => {
+  test('direction is vertical', () => {
     const wrapper = mount(FLayout, {
       props: { direction: 'vertical' }
     })
     expect(wrapper.classes()).toContain('f-layout__vertical')
   })
 
-  test('slot', () => {
+  test('direction is horizontal', () => {
     const wrapper = mount(FLayout, {
+      props: { direction: 'horizontal' }
+    })
+    expect(wrapper.classes()).not.toContain('f-layout__horizontal')
+  })
+
+  test('direction is vertical when render slot with FHeader component', () => {
+    const wrapper = mount(FLayout, {
+      slots: { default: FHeader }
+    })
+    expect(wrapper.classes()).toContain('f-layout__vertical')
+  })
+
+  test('direction is vertical when render slot with FFooter component', () => {
+    const wrapper = mount(FLayout, {
+      slots: { default: FFooter }
+    })
+    expect(wrapper.classes()).toContain('f-layout__vertical')
+  })
+
+  test('slot', () => {
+    const wrapper = mount(FLayout, { 
       slots: { default: 'hello' }
     })
     expect(wrapper.text()).toBe('hello')
