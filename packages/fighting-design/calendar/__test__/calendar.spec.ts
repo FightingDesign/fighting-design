938a97a59762c78523548c6a688d599096439206
Tyh2001 <1469442737@qq.com>
2024-05-06 17:32:37 +0800

    test: 更新日历组件单元测试

--- packages/fighting-design/calendar/__test__/calendar.spec.ts
+++ packages/fighting-design/calendar/__test__/calendar.spec.ts
@@ -10,4 +10,52 @@ describe('Calendar', () => {
       })
     expect(wrapper.classes()).toContain('f-calendar')
   })
+
+  test('border', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), border: true }
+      })
+    expect(wrapper.classes()).toContain('f-calendar__border')
+  })
+
+  test('borderColor', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), border: true, borderColor: 'red' }
+      })
+    expect(wrapper.attributes('style')).toContain('--calendar-border-color: red')
+  })
+
+  test('dayCellHeight', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), dayCellHeight: '20px' }
+      })
+    expect(wrapper.attributes('style')).toContain('--calendar-day-cell-height: 20px')
+  })
+
+  test('dayCellHeight', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), dayCellHeight: 44 }
+      })
+    expect(wrapper.attributes('style')).toContain('--calendar-day-cell-height: 44px')
+  })
+
+  test('weekCellHeight', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), weekCellHeight: '20px' }
+      })
+    expect(wrapper.attributes('style')).toContain('--calendar-week-cell-height: 20px')
+  })
+
+  test('weekCellHeight', () => {
+    const wrapper = mount(FCalendar,
+      {
+        props: { date: new Date(), weekCellHeight: 44 }
+      })
+    expect(wrapper.attributes('style')).toContain('--calendar-week-cell-height: 44px')
+  })
 })
