c4120b283f02f74df4d32af59fb3a4c8017612f1
w2xi <43wangxi@gmail.com>
2024-05-07 21:13:57 +0800

    test(components): [dropdown-item] cover remaining props

--- packages/fighting-design/dropdown-item/__test__/dropdown-item.spec.ts
+++ packages/fighting-design/dropdown-item/__test__/dropdown-item.spec.ts
@@ -10,16 +10,10 @@ describe('DropdownItem', () => {
   })
 
   test('disabled', () => {
-    const wrapper = mount(FDropdownItem, {
-      props: { disabled: true }
-    })
-    expect(wrapper.classes()).toContain('f-dropdown-item__disabled')
-  })
-
-  test('onClick', async () => {
     const fn = vi.fn()
     const wrapper = mount(FDropdownItem, {
       props: {
+        disabled: true,
         onClick: () => {
           fn('onClick')
         }
@@ -34,8 +28,50 @@ describe('DropdownItem', () => {
         }
       }
     })
+    expect(wrapper.classes()).toContain('f-dropdown-item__disabled')
+
     wrapper.trigger('click')
-    expect(fn.mock.calls[0][0]).toBe('close')
-    expect(fn.mock.calls[1][0]).toBe('onClick')
+    expect(fn.mock.calls[0]).not.toEqual(['close'])
+    expect(fn.mock.calls[1]).not.toEqual(['onClick'])
+  })
+
+  test('onClick', async () => {
+    const fn = vi.fn()
+    const wrapper = mount(FDropdownItem, {
+      props: {
+        onClick() {
+          fn('onClick')
+        }
+      },
+    })
+    wrapper.trigger('click')
+    expect(fn.mock.calls[0]).toEqual(['onClick'])
+  })
+
+  test('close callback should be triggered', async () => {
+    const fn = vi.fn()
+    const wrapper = mount(FDropdownItem, {
+      global: {
+        provide: {
+          [TRIGGER_CLOSE_KEY as symbol]: {
+            close() {
+              fn('close')
+            }
+          }
+        }
+      }
+    })
+    wrapper.trigger('click')
+    expect(fn.mock.calls[0]).toEqual(['close'])
+  })
+
+  test('should render slot', () => {
+    const watermelon = '🍉🍉🍉'
+    const wrapper = mount(FDropdownItem, {
+      slots: {
+        default: watermelon
+      }
+    })
+    expect(wrapper.text()).toContain(watermelon)
   })
 })
