47aef38d8f03f8e56bbfada77b5c2578dab2777e
Tyh2001 <Tyh2001@users.noreply.github.com>
2024-05-07 13:23:49 +0000

    deploy: ed412e276e803d812863a16f49a547eacd9e9f09

---
+++ assets/chunks/copy.36419be3.js
@@ -0,0 +1,1 @@
+import{F as a}from"./index.2be294a3.js";const r=o=>{navigator.clipboard.writeText(o),a({message:"复制成功",type:"primary"})};export{r as o};
--- assets/chunks/copy.dfb8ba0e.js
+++
@@ -1,1 +0,0 @@
-import{F as a}from"./index.3bf5ad84.js";const r=o=>{navigator.clipboard.writeText(o),a({message:"复制成功",type:"primary"})};export{r as o};
