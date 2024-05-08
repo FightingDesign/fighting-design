99d65699b384842e97fd256b6aa0ea84dfa77963
Tyh2001 <1469442737@qq.com>
2024-05-08 20:27:18 +0800

    test: 更新单元测试 修复 ts 类型错误

---
+++ packages/fighting-design/_utils/compatible/__test__/utils.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { objectEntries } from '..'
+
+const data = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+test('objectEntries', () => {
+  /**
+  * @see toStrictEqual https://cn.vitest.dev/api/expect.html#tostrictequal
+  */
+  expect(objectEntries(data)).toStrictEqual([
+    ['a', 1],
+    ['b', 2],
+    ['c', 3]
+  ])
+})
