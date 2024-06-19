ee2ad4040f11846b50c2b0c1845752c38fcc429d
Tyh2001 <1469442737@qq.com>
2024-06-19 19:38:13 +0800

    test: test

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <!-- <f-textarea v-model="value5" placeholder="禁止拉伸" resize="none" auto-height /> -->
-  <!-- <f-textarea v-model="value5" placeholder="禁止拉伸" resize="none" /> -->
-  <f-textarea clear v-model="value6" placeholder="随意拉伸" resize="both" />
-  <f-textarea v-model="value7" placeholder="水平拉伸" resize="horizontal" />
-  <f-textarea v-model="value8" placeholder="垂直拉伸" resize="vertical" />
+  <h1>{{ value || '暂无' }}</h1>
+  <f-select v-model="value" placeholder="请选择……">
+    <f-option :value="1">香蕉</f-option>
+    <f-option :value="2">苹果</f-option>
+    <f-option :value="3">哈密瓜</f-option>
+    <f-option :value="4">樱桃</f-option>
+  </f-select>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue'
 
-  const value5 = ref('none 禁止拉伸')
-  const value6 = ref('both 随意拉伸')
-  const value7 = ref('horizontal 水平拉伸')
-  const value8 = ref('vertical 垂直拉伸')
+  const value = ref('')
+
+  setTimeout(() => {
+    value.value = ''
+  }, 2000)
 </script>
